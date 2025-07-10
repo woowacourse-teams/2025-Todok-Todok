2337c11139b684401e94564dbe3c80f9e97f31c9
ljhee92 <weilainib7@gmail.com>
2025-07-10 15:04:13 +0900

    init

---
+++ backend/src/test/java/com/todoktodok/backend/TodokTodokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoktodok.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodokTodokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
