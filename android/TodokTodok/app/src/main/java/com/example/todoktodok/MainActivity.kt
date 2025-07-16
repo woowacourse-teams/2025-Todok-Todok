e4d77443f1cffd37dadbc0d1952cb49727b7765a
윤동현 <donghyunyoon81@gmail.com>
2025-07-16 14:35:14 +0900

    feat: #1 바텀 네비게이션 기능 추가

--- android/TodokTodok/app/src/main/java/com/example/todoktodok/MainActivity.kt
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/MainActivity.kt
@@ -5,16 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.todoktodok.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        binding.bnvContainer.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_note -> {}
+                R.id.menu_discussion -> {}
+                R.id.menu_library -> {}
+            }
+            true
         }
     }
 }
