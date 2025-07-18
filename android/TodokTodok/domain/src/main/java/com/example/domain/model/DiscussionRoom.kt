787ff89ada907c2f975c67e7092f917bae2ae193
chanho0908 <uni070@naver.com>
2025-07-18 15:34:49 +0900

    feat: #16 토론방 도메인 모델 추가

---
+++ android/TodokTodok/domain/src/main/java/com/example/domain/model/DiscussionRoom.kt
@@ -0,0 +1,13 @@
+package com.example.domain.model
+
+import com.example.domain.model.member.User
+import java.time.LocalDateTime
+
+data class DiscussionRoom(
+    val id: Long,
+    val discussionTitle: String,
+    val writer: User,
+    val createAt: LocalDateTime,
+    val snap: String,
+    val discussionOpinion: String,
+)
