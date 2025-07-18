1b3bf8834cc1df8cc12e27aa64e90ef74d8acd55
chanho0908 <uni070@naver.com>
2025-07-18 18:59:38 +0900

    feat: #5 도서 정보 목록 가져오기 구현 (data Layer)
    도서 데이터를 가져오기 위해 다음을 추가했습니다.
    - BookDataSource 클래스
    - BookRepository 인터페이스
    - BookRepositoryImpl 클래스

---
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/data/repository/BookRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.example.todoktodok.data.repository
+
+import com.example.domain.model.Book
+import com.example.domain.repository.BookRepository
+import com.example.todoktodok.data.datasource.BookDataSource
+
+class BookRepositoryImpl(
+    private val bookDataSource: BookDataSource,
+) : BookRepository {
+    override suspend fun getBooks(): List<Book> = bookDataSource.getBooks()
+}
