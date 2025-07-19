3c8acd46b0feefd96bcb17d000dec3a6520672ca
ljhee92 <weilainib7@gmail.com>
2025-07-19 22:54:13 +0900

    feat: 내 서재 도서 전체 조회 기능 구현

--- backend/src/main/java/todoktodok/backend/shelf/domain/Shelf.java
+++ backend/src/main/java/todoktodok/backend/shelf/domain/Shelf.java
@@ -1,17 +1,20 @@
-package todoktodok.backend.book.domain;
+package todoktodok.backend.shelf.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
+import todoktodok.backend.book.domain.Book;
 import todoktodok.backend.global.common.TimeStamp;
+import todoktodok.backend.member.domain.Member;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -19,26 +22,16 @@ import todoktodok.backend.global.common.TimeStamp;
 
 @Entity
 @SQLRestriction("deleted_at is NULL")
-@SQLDelete(sql = "UPDATE book SET deleted_at = NOW() WHERE id = ?")
-public class Book extends TimeStamp {
+@SQLDelete(sql = "UPDATE shelf SET deleted_at = NOW() WHERE id = ?")
+public class Shelf extends TimeStamp {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String title;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
 
-    private String summary;
-
-    @Column(nullable = false)
-    private String author;
-
-    @Column(nullable = false)
-    private String publisher;
-
-    @Column(nullable = false)
-    private String isbn;
-
-    private String image;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Book book;
 }
