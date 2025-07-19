d49fdaf6f543d438865a622774c4ffd4a0120def
Chaeyoung714 <202102760@hufs.ac.kr>
2025-07-19 16:51:41 +0900

    fix: MemberArgumentResolver 로직 수정

--- backend/src/main/java/todoktodok/backend/global/config/WebMvcConfig.java
+++ backend/src/main/java/todoktodok/backend/global/config/WebMvcConfig.java
@@ -1,16 +1,20 @@
 package todoktodok.backend.global.config;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import todoktodok.backend.global.interceptor.AuthorizationInterceptor;
+import todoktodok.backend.global.resolver.MemberArgumentResolver;
 
 @Configuration
 @AllArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final AuthorizationInterceptor authorizationInterceptor;
+    private final MemberArgumentResolver memberArgumentResolver;
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
@@ -22,5 +26,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
                         "/v3/api-docs/**",
                         "/h2-console"
                 );
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberArgumentResolver);
     }
 }
