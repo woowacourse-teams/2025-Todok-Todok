d26f1c7eb55fda32d6804ea1ee8534697d2d76ec
chanho0908 <uni070@naver.com>
2025-07-20 14:46:16 +0900

    chore : #5 ktlint 적용

--- android/TodokTodok/app/src/main/java/com/example/todoktodok/presentation/core/event/Event.kt
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/presentation/core/event/Event.kt
@@ -3,21 +3,22 @@ package com.example.todoktodok.presentation.core.event
 /**
  * Used as a wrapper for data that is exposed via a LiveData that represents an event.
  */
-open class Event<out T>(private val content: T) {
+open class Event<out T>(
+    private val content: T,
+) {
     var hasBeenHandled = false
         private set // Allow external read but not write
 
     /**
      * Returns the content and prevents its use again.
      */
-    fun getContentIfNotHandled(): T? {
-        return if (hasBeenHandled) {
+    fun getContentIfNotHandled(): T? =
+        if (hasBeenHandled) {
             null
         } else {
             hasBeenHandled = true
             content
         }
-    }
 
     /**
      * Returns the content, even if it's already been handled.
