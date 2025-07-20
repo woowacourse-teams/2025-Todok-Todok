11adee175958d7e51a1e2bbeff9d9266b9287b93
BWY <quantumjump10@gmail.com>
2025-07-21 02:08:15 +0900

    test: 토론방 생성 테스트 추가

---
+++ backend/src/test/java/todoktodok/backend/discussion/application/service/command/DiscussionCommandServiceTest.java
@@ -0,0 +1,85 @@
+package todoktodok.backend.discussion.application.service.command;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import todoktodok.backend.DatabaseInitializer;
+import todoktodok.backend.InitializerTimer;
+import todoktodok.backend.discussion.application.dto.request.DiscussionRequest;
+import todoktodok.backend.member.domain.repository.MemberRepository;
+
+@ActiveProfiles("test")
+@Transactional
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@ContextConfiguration(initializers = InitializerTimer.class)
+class DiscussionCommandServiceTest {
+
+    @Autowired
+    private DatabaseInitializer databaseInitializer;
+
+    @Autowired
+    private DiscussionCommandService discussionCommandService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        databaseInitializer.clear();
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 회원으로 토론방 생성 시 예외가 발생한다")
+    void createDiscussion_memberNotFound_fail() {
+        //given
+        databaseInitializer.setUserInfo();
+        databaseInitializer.setBookInfo();
+
+        final Long memberId = 999L;
+
+        final DiscussionRequest discussionRequest = new DiscussionRequest(
+                1L,
+                "이 책의 의존성 주입 방식에 대한 생각",
+                "스프링의 DI 방식은 유지보수에 정말 큰 도움이 된다고 느꼈습니다."
+        );
+
+        // when - then
+        Assertions.assertThatThrownBy(
+                        () -> discussionCommandService.createDiscussion(memberId, discussionRequest)
+                )
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 회원을 찾을 수 없습니다");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 기록으로 토론방 생성 시 예외가 발생한다")
+    void createDiscussion_NoteNotFound_fail() {
+        //given
+        databaseInitializer.setUserInfo();
+        databaseInitializer.setBookInfo();
+        databaseInitializer.setNoteInfo();
+
+        final Long memberId = 1L;
+        final Long noteId = 999L;
+
+        final DiscussionRequest discussionRequest = new DiscussionRequest(
+                noteId,
+                "이 책의 의존성 주입 방식에 대한 생각",
+                "스프링의 DI 방식은 유지보수에 정말 큰 도움이 된다고 느꼈습니다."
+        );
+
+        // when - then
+        Assertions.assertThatThrownBy(
+                        () -> discussionCommandService.createDiscussion(memberId, discussionRequest)
+                )
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 기록을 찾을 수 없습니다");
+    }
+}
---
+++ backend/src/test/java/todoktodok/backend/discussion/domain/DiscussionTest.java
@@ -0,0 +1,129 @@
+package todoktodok.backend.discussion.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import todoktodok.backend.book.domain.Book;
+import todoktodok.backend.member.domain.Member;
+import todoktodok.backend.note.domain.Note;
+
+class DiscussionTest {
+
+    @Test
+    @DisplayName("토론방 제목이 비어있으면 예외가 발생한다")
+    void validateTitle_tooShortTitle_fail() {
+        // given
+        final Member member = getMember();
+        final Book book = getBook();
+        final Note note = getNote(book, member);
+
+        final String title = "";
+
+        // when - then
+        Assertions.assertThatThrownBy(
+                        () -> Discussion.builder()
+                                .title(title)
+                                .content("클린한 코드를 작성해봅시다")
+                                .member(member)
+                                .book(book)
+                                .note(note)
+                                .build()
+                ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("토론방 제목은 1자 이상, 50자 이하여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("토론방 제목이 50자를 초과하면 예외가 발생한다")
+    void validateTitle_tooLongTitle_fail() {
+        // given
+        final Member member = getMember();
+        final Book book = getBook();
+        final Note note = getNote(book, member);
+
+        final String title = "a".repeat(51);
+
+        // when - then
+        Assertions.assertThatThrownBy(
+                        () -> Discussion.builder()
+                                .title(title)
+                                .content("클린한 코드를 작성해봅시다")
+                                .member(member)
+                                .book(book)
+                                .note(note)
+                                .build()
+                ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("토론방 제목은 1자 이상, 50자 이하여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("토론방 내용이 2500자를 초과하면 예외가 발생한다.")
+    void validateContent_tooLongContent_fail() {
+        // given
+        final Member member = getMember();
+        final Book book = getBook();
+        final Note note = getNote(book, member);
+
+        final String content = "a".repeat(2501);
+
+        // when - then
+        Assertions.assertThatThrownBy(
+                        () -> Discussion.builder()
+                                .title("클린코드")
+                                .content(content)
+                                .member(member)
+                                .book(book)
+                                .note(note)
+                                .build()
+                ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("토론방 내용은 1자 이상, 2500자 이하여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("토론방 내용이 비어 있으면 예외가 발생한다")
+    void validateContent_TooShortContent_fail() {
+        // given
+        final Member member = getMember();
+        final Book book = getBook();
+        final Note note = getNote(book, member);
+
+        final String content = "";
+
+        // when - then
+        Assertions.assertThatThrownBy(
+                        () -> Discussion.builder()
+                                .title("클린코드")
+                                .content(content)
+                                .member(member)
+                                .book(book)
+                                .note(note)
+                                .build()
+                ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("토론방 내용은 1자 이상, 2500자 이하여야 합니다.");
+    }
+
+    private Note getNote(Book book, Member member) {
+        return Note.builder()
+                .snap("클린코드")
+                .memo("로버트마틴")
+                .book(book)
+                .member(member)
+                .build();
+    }
+
+    private Book getBook() {
+        return Book.builder()
+                .title("클린코드")
+                .author("로버트마틴")
+                .publisher("피어슨")
+                .isbn("123")
+                .build();
+    }
+
+    private Member getMember() {
+        return Member.builder()
+                .email("user@gmail.com")
+                .nickname("user")
+                .profileImage("https://image.jpg")
+                .build();
+    }
+}
