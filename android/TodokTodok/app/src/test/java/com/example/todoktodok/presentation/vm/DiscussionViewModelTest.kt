31d74d494a27c1e12fdfd46089554352b0c6cd1f
윤동현 <donghyunyoon81@gmail.com>
2025-07-20 16:18:07 +0900

    test: #54 테스트용 확장 함수 위치 변경

--- android/TodokTodok/app/src/test/java/com/example/todoktodok/presentation/vm/DiscussionViewModelTest.kt
+++ android/TodokTodok/app/src/test/java/com/example/todoktodok/presentation/vm/DiscussionViewModelTest.kt
@@ -1,9 +1,9 @@
 package com.example.todoktodok.presentation.vm
 
 import com.example.todoktodok.DISCUSSION_ROOMS
+import com.example.todoktodok.InstantTaskExecutorExtension
+import com.example.todoktodok.ext.getOrAwaitValue
 import com.example.todoktodok.fake.FakeDiscussionRoomRepository
-import com.example.todoktodok.presentation.InstantTaskExecutorExtension
-import com.example.todoktodok.presentation.getOrAwaitValue
 import com.example.todoktodok.presentation.view.discussion.vm.DiscussionViewModel
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
