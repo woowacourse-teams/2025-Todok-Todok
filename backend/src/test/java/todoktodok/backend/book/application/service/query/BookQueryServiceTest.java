d418c4e788c77c5660e2edf5d47f2c52f66739ad
ljhee92 <weilainib7@gmail.com>
2025-07-20 17:03:38 +0900

    style: 미사용 import 제거

--- backend/src/test/java/todoktodok/backend/book/application/service/query/BookQueryServiceTest.java
+++ backend/src/test/java/todoktodok/backend/book/application/service/query/BookQueryServiceTest.java
@@ -6,8 +6,6 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
