a32be7b68d3dc2ed1529c9df55271fb822115a63
윤동현 <donghyunyoon81@gmail.com>
2025-07-20 20:12:53 +0900

    feat: #10 토론방 관련 DataSource 및 Repository 모듈 추가

--- android/TodokTodok/app/src/main/java/com/example/todoktodok/data/di/DataSourceModule.kt
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/data/di/DataSourceModule.kt
@@ -1,7 +1,11 @@
 package com.example.todoktodok.data.di
 
 import com.example.todoktodok.data.datasource.BookDataSource
+import com.example.todoktodok.data.datasource.DiscussionRoomDataSource
+import com.example.todoktodok.data.datasource.DiscussionRoomDataSourceImpl
 
 class DataSourceModule {
     val bookDataSource: BookDataSource by lazy { BookDataSource() }
+
+    val discussionRoomDataSource: DiscussionRoomDataSource by lazy { DiscussionRoomDataSourceImpl() }
 }
--- android/TodokTodok/app/src/main/java/com/example/todoktodok/data/di/RepositoryModule.kt
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/data/di/RepositoryModule.kt
@@ -1,11 +1,19 @@
 package com.example.todoktodok.data.di
 
 import com.example.domain.repository.BookRepository
+import com.example.domain.repository.DiscussionRoomRepository
 import com.example.todoktodok.data.repository.BookRepositoryImpl
+import com.example.todoktodok.data.repository.DiscussionRoomRepositoryImpl
 import kotlin.getValue
 
 class RepositoryModule(
     dataSourceModule: DataSourceModule,
 ) {
     val bookRepository: BookRepository by lazy { BookRepositoryImpl(dataSourceModule.bookDataSource) }
+
+    val discussionRoomRepository: DiscussionRoomRepository by lazy {
+        DiscussionRoomRepositoryImpl(
+            dataSourceModule.discussionRoomDataSource
+        )
+    }
 }
