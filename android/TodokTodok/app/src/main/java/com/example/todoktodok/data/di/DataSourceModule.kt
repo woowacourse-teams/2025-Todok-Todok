9c76f0fbc133f76854b5bfa559841f4c562dfdb3
윤동현 <donghyunyoon81@gmail.com>
2025-07-21 21:06:12 +0900

    Refactor: DefaultDiscussionRoomDataSource import 경로 변경

--- android/TodokTodok/app/src/main/java/com/example/todoktodok/data/di/DataSourceModule.kt
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/data/di/DataSourceModule.kt
@@ -1,8 +1,8 @@
 package com.example.todoktodok.data.di
 
 import com.example.todoktodok.data.datasource.BookDataSource
-import com.example.todoktodok.data.datasource.DiscussionRoomDataSource
 import com.example.todoktodok.data.datasource.DefaultDiscussionRoomDataSource
+import com.example.todoktodok.data.datasource.DiscussionRoomDataSource
 
 class DataSourceModule {
     val bookDataSource: BookDataSource by lazy { BookDataSource() }
