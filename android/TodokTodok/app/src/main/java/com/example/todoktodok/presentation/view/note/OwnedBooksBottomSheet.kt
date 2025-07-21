e39f48e886f922c9d32ccd41ba84a13d7fd83ab4
chanho0908 <uni070@naver.com>
2025-07-21 13:39:31 +0900

    feat: #12 노트 화면에서 사용할 책 목록 BottomSheet 추가

---
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/presentation/view/note/OwnedBooksBottomSheet.kt
@@ -0,0 +1,52 @@
+package com.example.todoktodok.presentation.view.note
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import com.example.todoktodok.R
+import com.example.todoktodok.databinding.OwnedBooksBottomSheetBinding
+import com.example.todoktodok.presentation.core.ext.getParcelableArrayListCompat
+import com.example.todoktodok.presentation.view.library.BooksAdapter
+import com.example.todoktodok.presentation.view.note.NoteFragment.Companion.REQUEST_KEY
+import com.example.todoktodok.presentation.view.parcelable.ParcelableBook
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class OwnedBooksBottomSheet : BottomSheetDialogFragment(R.layout.owned_books_bottom_sheet) {
+    private val booksAdapter: BooksAdapter = BooksAdapter()
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = OwnedBooksBottomSheetBinding.bind(view)
+
+        // TODO(리사이클러뷰 아이템 이벤트로 수정)
+        binding.root.setOnClickListener {
+            setFragmentResult(REQUEST_KEY, bundleOf("selectedBook" to "da"))
+            dismiss()
+        }
+        initView(binding)
+    }
+
+    fun initView(binding: OwnedBooksBottomSheetBinding) {
+        binding.rvBooks.adapter = booksAdapter
+
+        val books = requireArguments().getParcelableArrayListCompat<ParcelableBook>(KEY_BOOKS)
+        booksAdapter.submitList(books.map { it.toDomain() })
+    }
+
+    companion object {
+        fun newInstance(books: List<ParcelableBook>): OwnedBooksBottomSheet =
+            OwnedBooksBottomSheet().apply {
+                arguments =
+                    Bundle().apply {
+                        putParcelableArrayList(KEY_BOOKS, ArrayList(books))
+                    }
+            }
+
+        private const val KEY_BOOKS = "books"
+        private const val RESULT_KEY = "selectedBook"
+    }
+}
