e0fa29f34021072ab42acee4f1fe3ef65ae948e1
윤동현 <donghyunyoon81@gmail.com>
2025-07-21 22:22:30 +0900

    feat: #61 댓글 기능 관련 도메인 및 데이터 계층 추가

---
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/data/repository/DefaultCommentRepository.kt
@@ -0,0 +1,11 @@
+package com.example.todoktodok.data.repository
+
+import com.example.domain.model.Comment
+import com.example.domain.repository.CommentRepository
+import com.example.todoktodok.data.datasource.CommentDataSource
+
+class DefaultCommentRepository(private val commentDataSource: CommentDataSource) :
+    CommentRepository {
+    override fun getCommentsByDiscussionRoomId(id: Long): List<Comment> =
+        commentDataSource.getCommentsByDiscussionRoomId(id)
+}
