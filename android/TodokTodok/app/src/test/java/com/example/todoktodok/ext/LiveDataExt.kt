3fe54d26d09f9efc4bb2929db4ca5578049fae49
윤동현 <donghyunyoon81@gmail.com>
2025-07-21 16:42:05 +0900

    test: #10 DiscussionViewModelTest 테스트 추가

--- android/TodokTodok/app/src/test/java/com/example/todoktodok/ext/LiveDataExt.kt
+++ android/TodokTodok/app/src/test/java/com/example/todoktodok/ext/LiveDataExt.kt
@@ -1,7 +1,11 @@
 package com.example.todoktodok.ext
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import com.example.todoktodok.presentation.core.event.SingleLiveData
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
@@ -30,3 +34,32 @@ fun <T> LiveData<T>.getOrAwaitValue(
     @Suppress("UNCHECKED_CAST")
     return data as T
 }
+
+fun <T> SingleLiveData<T>.getOrAwaitValue(
+    time: Long = 2,
+    timeUnit: TimeUnit = TimeUnit.SECONDS,
+): T {
+    var data: T? = null
+    val latch = CountDownLatch(1)
+
+    val lifecycleOwner = object : LifecycleOwner {
+        private val registry = LifecycleRegistry(this)
+        override val lifecycle: Lifecycle
+            get() = registry.apply {
+                handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            }
+    }
+
+    this.observe(lifecycleOwner) {
+        data = it
+        latch.countDown()
+    }
+
+    if (!latch.await(time, timeUnit)) {
+        throw TimeoutException("SingleLiveData value was never set.")
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    return data as T
+}
+
--- android/TodokTodok/app/src/test/java/com/example/todoktodok/presentation/vm/DiscussionViewModelTest.kt
+++ android/TodokTodok/app/src/test/java/com/example/todoktodok/presentation/vm/DiscussionViewModelTest.kt
@@ -4,6 +4,7 @@ import com.example.todoktodok.DISCUSSION_ROOMS
 import com.example.todoktodok.InstantTaskExecutorExtension
 import com.example.todoktodok.ext.getOrAwaitValue
 import com.example.todoktodok.fake.FakeDiscussionRoomRepository
+import com.example.todoktodok.presentation.view.discussion.DiscussionUiEvent
 import com.example.todoktodok.presentation.view.discussion.vm.DiscussionViewModel
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -20,10 +21,30 @@ class DiscussionViewModelTest {
     }
 
     @Test
-    fun `토론방 초기화 테스트`() {
+    fun `저장소에서 불러온 토론방 목록으로 값을 초기화 한다`() {
         //given
         val expected = DISCUSSION_ROOMS
         //then
         assertThat(discussionViewModel.discussionRooms.getOrAwaitValue()).isEqualTo(expected)
+    }
+
+    @Test
+    fun `UiEvent를 NavigateAddDiscussionRoom으로 변경한다`() {
+        //given
+        val expected = DiscussionUiEvent.NavigateAddDiscussionRoom
+        //when
+        discussionViewModel.onUiEvent(DiscussionUiEvent.NavigateAddDiscussionRoom)
+        //then
+        assertThat(discussionViewModel.uiEvent.getOrAwaitValue()).isEqualTo(expected)
+    }
+
+    @Test
+    fun `UiEvent를 NavigateDiscussionRoom으로 변경한다`() {
+        //given
+        val expected = DiscussionUiEvent.NavigateDiscussionRoom(1)
+        //when
+        discussionViewModel.onUiEvent(DiscussionUiEvent.NavigateDiscussionRoom(1))
+        //then
+        assertThat(discussionViewModel.uiEvent.getOrAwaitValue()).isEqualTo(expected)
     }
 }
