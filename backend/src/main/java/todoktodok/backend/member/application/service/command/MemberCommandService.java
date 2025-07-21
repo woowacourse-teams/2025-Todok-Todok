5572691e4621e7f269c504d03ae492c825630aa6
Chaeyoung714 <202102760@hufs.ac.kr>
2025-07-21 16:52:26 +0900

    feat: 신고 및 차단 시 예외 상황 추가

--- backend/src/main/java/todoktodok/backend/member/application/service/command/MemberCommandService.java
+++ backend/src/main/java/todoktodok/backend/member/application/service/command/MemberCommandService.java
@@ -50,14 +50,12 @@ public class MemberCommandService {
             final Long memberId,
             final Long targetId
     ) {
-        if (memberId.equals(targetId)) {
-            throw new IllegalArgumentException("자기 자신을 차단할 수 없습니다");
-        }
+        validateSelfBlock(memberId, targetId);
 
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoSuchElementException("해당하는 회원을 찾을 수 없습니다"));
-        Member target = memberRepository.findById(targetId)
-                .orElseThrow(() -> new NoSuchElementException("해당하는 회원을 찾을 수 없습니다"));
+        Member member = getMember(memberId);
+        Member target = getMember(targetId);
+
+        validateDuplicatedBlock(member,target);
 
         Block block = Block.builder()
                 .member(member)
@@ -70,14 +68,12 @@ public class MemberCommandService {
             final Long memberId,
             final Long targetId
     ) {
-        if (memberId.equals(targetId)) {
-            throw new IllegalArgumentException("자기 자신을 신고할 수 없습니다");
-        }
+        validateSelfReport(memberId, targetId);
 
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoSuchElementException("해당하는 회원을 찾을 수 없습니다"));
-        Member target = memberRepository.findById(targetId)
-                .orElseThrow(() -> new NoSuchElementException("해당하는 회원을 찾을 수 없습니다"));
+        Member member = getMember(memberId);
+        Member target = getMember(targetId);
+
+        validateDuplicatedReport(member, target);
 
         MemberReport memberReport = MemberReport.builder()
                 .member(member)
@@ -89,6 +85,35 @@ public class MemberCommandService {
     private void validateDuplicatedNickname(final SignupRequest signupRequest) {
         if (memberRepository.existsByNickname(signupRequest.nickname())) {
             throw new IllegalArgumentException("이미 존재하는 닉네임입니다");
+        }
+    }
+
+    private Member getMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new NoSuchElementException("해당하는 회원을 찾을 수 없습니다"));
+    }
+
+    private static void validateSelfBlock(Long memberId, Long targetId) {
+        if (memberId.equals(targetId)) {
+            throw new IllegalArgumentException("자기 자신을 차단할 수 없습니다");
+        }
+    }
+
+    private void validateDuplicatedBlock(Member member, Member target) {
+        if (blockRepository.existsByMemberAndTarget(member, target)) {
+            throw new IllegalArgumentException("이미 차단한 회원입니다");
+        }
+    }
+
+    private void validateDuplicatedReport(Member member, Member target) {
+        if (blockRepository.existsByMemberAndTarget(member, target)) {
+            throw new IllegalArgumentException("이미 신고한 회원입니다");
+        }
+    }
+
+    private static void validateSelfReport(Long memberId, Long targetId) {
+        if (memberId.equals(targetId)) {
+            throw new IllegalArgumentException("자기 자신을 신고할 수 없습니다");
         }
     }
 }
