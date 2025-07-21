aebc8689b53ae5237f7e0d3ae9871f6d9d63bc50
BWY <quantumjump10@gmail.com>
2025-07-21 10:35:52 +0900

    refactor: 지연로딩 설정 추가

--- backend/src/main/java/todoktodok/backend/discussion/domain/Discussion.java
+++ backend/src/main/java/todoktodok/backend/discussion/domain/Discussion.java
@@ -2,6 +2,7 @@ package todoktodok.backend.discussion.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -41,15 +42,15 @@ public class Discussion extends TimeStamp {
     @Column(nullable = false, length = 512)
     private String content;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
     private Book book;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Note note;
 
     @Builder
