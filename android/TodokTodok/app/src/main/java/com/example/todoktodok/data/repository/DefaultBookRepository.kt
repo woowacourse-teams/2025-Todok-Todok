c6b88d2cb7a6f7d2097a26c9d1ad954b01e9ee08
chanho0908 <uni070@naver.com>
2025-07-23 07:24:02 +0900

    feat: #12 책 목록 일급 컬렉션 추가

--- android/TodokTodok/app/src/main/java/com/example/todoktodok/data/repository/DefaultBookRepository.kt
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/data/repository/DefaultBookRepository.kt
@@ -1,11 +1,11 @@
 package com.example.todoktodok.data.repository
 
-import com.example.domain.model.Book
+import com.example.domain.model.Books
 import com.example.domain.repository.BookRepository
 import com.example.todoktodok.data.datasource.book.BookDataSource
 
 class DefaultBookRepository(
     private val remoteBookDataSource: BookDataSource,
 ) : BookRepository {
-    override suspend fun getBooks(): List<Book> = remoteBookDataSource.fetchBooks()
+    override suspend fun getBooks(): Books = remoteBookDataSource.fetchBooks()
 }
