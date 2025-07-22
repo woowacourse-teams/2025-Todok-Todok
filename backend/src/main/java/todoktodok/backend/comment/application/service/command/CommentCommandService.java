32099b8993eaea97c7cf2ffa22e2a3283964c15a
sonjh919 <kingjh1125@gmail.com>
2025-07-22 21:33:53 +0900

    feat: 댓글과 토론방 검증 로직 추가

--- backend/src/main/java/todoktodok/backend/comment/application/service/command/CommentCommandService.java
+++ backend/src/main/java/todoktodok/backend/comment/application/service/command/CommentCommandService.java
@@ -49,8 +49,10 @@ public class CommentCommandService {
     ) {
         final Member member = getMember(memberId);
         final Comment comment = getComment(commentId);
+        final Discussion discussion = getDiscussion(discussionId);
 
-        validateIsDiscussionExists(discussionId);
+        comment.validateMatchWithDiscussion(discussion);
+
         validateSelfReport(member, comment);
         validateDuplicatedReport(member, comment);
 
@@ -75,12 +77,6 @@ public class CommentCommandService {
     private Member getMember(final Long memberId) {
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new NoSuchElementException("해당 회원을 찾을 수 없습니다"));
-    }
-
-    private void validateIsDiscussionExists(final Long discussionId) {
-        if (!discussionRepository.existsById(discussionId)) {
-            throw new NoSuchElementException("해당 토론방을 찾을 수 없습니다");
-        }
     }
 
     private void validateSelfReport(
--- backend/src/main/java/todoktodok/backend/comment/domain/Comment.java
+++ backend/src/main/java/todoktodok/backend/comment/domain/Comment.java
@@ -58,6 +58,12 @@ public class Comment extends TimeStamp {
         );
     }
 
+    public void validateMatchWithDiscussion(final Discussion discussion) {
+        if (!this.discussion.equals(discussion)) {
+            throw new IllegalArgumentException("해당 토론방에 있는 댓글이 아닙니다");
+        }
+    }
+
     private static void validateContent(final String content) {
         if (content.isEmpty() || content.length() > CONTENT_MAX_LENGTH) {
             throw new IllegalArgumentException("댓글 내용은 1자 이상, 1500자 이하여야 합니다");
