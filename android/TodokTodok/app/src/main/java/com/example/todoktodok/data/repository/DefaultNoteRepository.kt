590ac2c93a1c7df3b400888a96f9876b65e04a90
chanho0908 <uni070@naver.com>
2025-07-23 12:48:51 +0900

    fix: #12 노트 저장 요청 시 도메인 모델 사용하지 않도록 변경

--- android/TodokTodok/app/src/main/java/com/example/todoktodok/data/repository/DefaultNoteRepository.kt
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/data/repository/DefaultNoteRepository.kt
@@ -1,14 +1,18 @@
 package com.example.todoktodok.data.repository
 
-import com.example.domain.model.Note
 import com.example.domain.repository.NoteRepository
 import com.example.todoktodok.data.datasource.note.NoteDataSource
-import com.example.todoktodok.data.network.request.toRequest
+import com.example.todoktodok.data.network.request.NoteRequest
 
 class DefaultNoteRepository(
     private val remoteNoteDataSource: NoteDataSource,
 ) : NoteRepository {
-    override suspend fun saveNote(note: Note) {
-        remoteNoteDataSource.saveNote(note.toRequest())
+    override suspend fun saveNote(
+        bookId: Long,
+        snap: String,
+        memo: String,
+    ) {
+        val request = NoteRequest(bookId, snap, memo)
+        remoteNoteDataSource.saveNote(request)
     }
 }
