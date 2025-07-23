3d85dabdf91d06c65cdf04ac091e751cd336107b
sonjh919 <kingjh1125@gmail.com>
2025-07-23 15:12:59 +0900

    test: 토론방별 댓글 목록 조회 테스트 추가

---
+++ backend/src/test/java/todoktodok/backend/comment/application/service/query/CommentQueryServiceTest.java
@@ -0,0 +1,64 @@
+package todoktodok.backend.comment.application.service.query;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import todoktodok.backend.DatabaseInitializer;
+import todoktodok.backend.InitializerTimer;
+
+@ActiveProfiles("test")
+@Transactional
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@ContextConfiguration(initializers = InitializerTimer.class)
+public class CommentQueryServiceTest {
+
+    @Autowired
+    private DatabaseInitializer databaseInitializer;
+
+    @Autowired
+    private CommentQueryService commentQueryService;
+
+    @BeforeEach
+    void setUp() {
+        databaseInitializer.clear();
+    }
+
+    @Test
+    @DisplayName("없는 토론방에 대해 댓글을 조회할 경우 예외가 발생한다")
+    void createCommentTest_discussionNotFound_fail() {
+        // given
+        databaseInitializer.setDefaultUserInfo();
+        databaseInitializer.setDefaultBookInfo();
+        databaseInitializer.setDefaultNoteInfo();
+
+        // when - then
+        assertThatThrownBy(() -> commentQueryService.getComments(1L, 1L))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("해당 토론방을 찾을 수 없습니다");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 회원으로 댓글을 작성할 경우 예외가 발생한다")
+    void createCommentTest_memberNotFound_fail() {
+        // given
+        databaseInitializer.setDefaultUserInfo();
+        databaseInitializer.setDefaultBookInfo();
+        databaseInitializer.setDefaultNoteInfo();
+        databaseInitializer.setDefaultDiscussionInfo();
+        databaseInitializer.setDefaultCommentInfo();
+
+        // when - then
+        assertThatThrownBy(() -> commentQueryService.getComments(2L, 1L))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("해당 회원을 찾을 수 없습니다");
+    }
+}
--- backend/src/test/java/todoktodok/backend/comment/presentation/CommentControllerTest.java
+++ backend/src/test/java/todoktodok/backend/comment/presentation/CommentControllerTest.java
@@ -79,4 +79,27 @@ public class CommentControllerTest {
                 .then().log().all()
                 .statusCode(HttpStatus.CREATED.value());
     }
+
+    @Test
+    @DisplayName("토론방별 댓글을 조회한다")
+    void getCommentsTest() {
+        // given
+        databaseInitializer.setDefaultUserInfo();
+        databaseInitializer.setDefaultBookInfo();
+        databaseInitializer.setDefaultNoteInfo();
+        databaseInitializer.setDefaultDiscussionInfo();
+
+        databaseInitializer.setCommentInfo("상속의 핵심 목적은 타입 계층의 구축입니다!", 1L, 1L);
+        databaseInitializer.setCommentInfo("조합은 재사용이 목적입니다!", 1L, 1L);
+
+        final String token = MemberFixture.login("user@gmail.com");
+
+        // when - then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .header("Authorization", token)
+                .when().get("/api/v1/discussions/1/comments")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
+    }
 }
