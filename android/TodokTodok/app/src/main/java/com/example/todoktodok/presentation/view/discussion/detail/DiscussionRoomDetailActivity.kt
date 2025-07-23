360d09b9e85110dc00a5e4c4c114bcb7f3b5befd
윤동현 <donghyunyoon81@gmail.com>
2025-07-23 13:09:06 +0900

    feat: #71 토론방 상세 화면 구현

---
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/presentation/view/discussion/detail/DiscussionRoomDetailActivity.kt
@@ -0,0 +1,24 @@
+package com.example.todoktodok.presentation.view.discussion.detail
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.todoktodok.R
+
+class DiscussionRoomDetailActivity : AppCompatActivity(R.layout.activity_discussion_room_detail) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
