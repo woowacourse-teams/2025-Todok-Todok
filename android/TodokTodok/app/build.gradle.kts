eb8efd08fecebe46ea132df4e72c3ec64e3edf54
chanho0908 <uni070@naver.com>
2025-07-23 17:48:57 +0900

    fix: #80 JVM 타겟 설정 방식 변경

--- android/TodokTodok/app/build.gradle.kts
+++ android/TodokTodok/app/build.gradle.kts
@@ -57,9 +57,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlinOptions {
-        jvmTarget = "21"
+    kotlin {
+        jvmToolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        }
     }
+
     packaging {
         resources {
             excludes += "META-INF/**"
