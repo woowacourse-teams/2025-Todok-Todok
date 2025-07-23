1a99966767af43b4dd3ec591335030f0c9b3fd04
윤동현 <donghyunyoon81@gmail.com>
2025-07-23 14:39:12 +0900

    feat: #71 Comment 관련 Repository 및 DataSource 추가

--- android/TodokTodok/app/src/main/java/com/example/todoktodok/data/di/RepositoryModule.kt
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/data/di/RepositoryModule.kt
@@ -1,12 +1,13 @@
 package com.example.todoktodok.data.di
 
 import com.example.domain.repository.BookRepository
+import com.example.domain.repository.CommentRepository
 import com.example.domain.repository.DiscussionRoomRepository
 import com.example.domain.repository.NoteRepository
 import com.example.todoktodok.data.repository.DefaultBookRepository
+import com.example.todoktodok.data.repository.DefaultCommentRepository
 import com.example.todoktodok.data.repository.DefaultDiscussionRoomRepository
 import com.example.todoktodok.data.repository.DefaultNoteRepository
-import kotlin.getValue
 
 class RepositoryModule(
     dataSourceModule: DataSourceModule,
@@ -19,4 +20,6 @@ class RepositoryModule(
     val bookRepository: BookRepository by lazy { DefaultBookRepository(dataSourceModule.bookDataSource) }
 
     val noteRepository: NoteRepository by lazy { DefaultNoteRepository(dataSourceModule.noteDataSource) }
+
+    val commentRepository: CommentRepository by lazy { DefaultCommentRepository(dataSourceModule.commentDataSource) }
 }
