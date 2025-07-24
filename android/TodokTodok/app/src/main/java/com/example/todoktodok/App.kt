d72d349c1b2deeeee6c684deb11f1fd6689f3b82
chanho0908 <uni070@naver.com>
2025-07-24 16:41:29 +0900

    feat: #110 AppContainer에서 TokenDataSource, AuthInterceptor 초기화 로직 추가

--- android/TodokTodok/app/src/main/java/com/example/todoktodok/App.kt
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/App.kt
@@ -4,6 +4,6 @@ import android.app.Application
 
 class App : Application() {
     val container: AppContainer by lazy {
-        AppContainer()
+        AppContainer(applicationContext)
     }
 }
---
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/data/datasource/token/TokenDataSource.kt
@@ -0,0 +1,28 @@
+package com.example.todoktodok.data.datasource.token
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import com.example.todoktodok.data.database.UserSetting
+import com.example.todoktodok.data.database.dataStore
+import kotlinx.coroutines.flow.first
+
+class TokenDataSource(
+    private val context: Context,
+) {
+    private val dataStore: DataStore<UserSetting>
+        get() = context.dataStore
+
+    suspend fun getAccessToken(): String = dataStore.data.first().accessToken
+
+    suspend fun saveToken(
+        accessToken: String,
+        refreshToken: String = "",
+    ) {
+        dataStore.updateData {
+            it.copy(
+                accessToken,
+                "", // 리프레시 토큰 구현
+            )
+        }
+    }
+}
