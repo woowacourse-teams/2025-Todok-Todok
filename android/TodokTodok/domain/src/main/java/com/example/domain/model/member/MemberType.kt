4e38d671d487e8fa2c87a1be335a7960fefc37a7
chanho0908 <uni070@naver.com>
2025-07-24 16:32:35 +0900

    feat: #110 회원 타입 enum 정의 및 테스트 코드 작성

---
+++ android/TodokTodok/domain/src/main/java/com/example/domain/model/member/MemberType.kt
@@ -0,0 +1,18 @@
+package com.example.domain.model.member
+
+enum class MemberType {
+    USER,
+    TEMP_USER,
+    ;
+
+    companion object {
+        fun MemberType(role: String): MemberType =
+            when (role) {
+                "USER" -> USER
+                "TEMP_USER" -> TEMP_USER
+                else -> throw IllegalArgumentException(INVALID_MEMBER_TYPE)
+            }
+
+        private const val INVALID_MEMBER_TYPE = "존재하지 않는 유저 타입입니다."
+    }
+}
