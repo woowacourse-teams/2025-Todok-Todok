4498acdd4c3aaeca80bc4649b18df34f63f406c8
chanho0908 <uni070@naver.com>
2025-07-24 11:01:34 +0900

    feat: #76 로그인 화면 기능 구현
    - ViewModel 및 ViewModelFactory 추가
    - GoogleLoginManager 연동 및 로그인 처리
    - 로그인 성공/실패 시 UI 업데이트 (Snackbar)
    - 관련 문자열 리소스 추가

---
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/presentation/view/start/vm/StartViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.todoktodok.presentation.view.start.vm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.domain.repository.MemberRepository
+
+class StartViewModelFactory(
+    private val memberRepository: MemberRepository,
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(StartViewModel::class.java)) {
+            return StartViewModel(memberRepository) as T
+        } else {
+            throw IllegalArgumentException()
+        }
+    }
+}
