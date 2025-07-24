3a8e7903d9deb65a280758a2aadfde692462ee5f
chanho0908 <uni070@naver.com>
2025-07-24 13:44:48 +0900

    fix: #76 StartActivity를 AuthActivity로 이름 변경
    - StartActivity 클래스 이름을 AuthActivity로 변경
    - 관련된 레이아웃 파일명 activity_start.xml을 activity_auth.xml로 변경
    - AndroidManifest.xml에서 액티비티 이름 변경

--- android/TodokTodok/app/src/main/java/com/example/todoktodok/presentation/view/start/AuthActivity.kt
+++ android/TodokTodok/app/src/main/java/com/example/todoktodok/presentation/view/start/AuthActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.todoktodok.R
 
-class StartActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_start)
+        setContentView(R.layout.activity_auth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
