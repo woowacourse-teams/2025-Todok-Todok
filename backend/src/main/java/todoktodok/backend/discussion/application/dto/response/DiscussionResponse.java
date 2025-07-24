7c5d5219bdfd4dd6be3affe30227ea6a63b936d5
BWY <quantumjump10@gmail.com>
2025-07-24 15:50:33 +0900

    refactor: DiscussionResponse 필드 수정

--- backend/src/main/java/todoktodok/backend/discussion/application/dto/response/DiscussionResponse.java
+++ backend/src/main/java/todoktodok/backend/discussion/application/dto/response/DiscussionResponse.java
@@ -1,11 +1,17 @@
 package todoktodok.backend.discussion.application.dto.response;
 
+import java.time.LocalDateTime;
 import todoktodok.backend.book.application.dto.response.BookResponse;
 import todoktodok.backend.discussion.domain.Discussion;
+import todoktodok.backend.member.application.dto.response.MemberResponse;
+import todoktodok.backend.note.application.dto.response.MyNoteResponse;
 
 public record DiscussionResponse(
         Long discussionId,
         BookResponse bookResponse,
+        MemberResponse memberResponse,
+        LocalDateTime createdAt,
+        MyNoteResponse noteResponse,
         String discussionTitle,
         String discussionOpinion
 ) {
@@ -13,6 +19,9 @@ public record DiscussionResponse(
         this(
                 discussion.getId(),
                 new BookResponse(discussion.getBook()),
+                new MemberResponse(discussion.getMember()),
+                discussion.getCreatedAt(),
+                new MyNoteResponse(discussion.getNote()),
                 discussion.getTitle(),
                 discussion.getContent()
         );
