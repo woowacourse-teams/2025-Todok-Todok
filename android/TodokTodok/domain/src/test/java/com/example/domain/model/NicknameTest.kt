12c1769e03a31840a2f32f5cdbd281a78f14edbc
chanho0908 <uni070@naver.com>
2025-07-24 21:09:52 +0900

    test: #110 닉네임 유효성 검사 테스트 케이스 추가

--- android/TodokTodok/domain/src/test/java/com/example/domain/model/NicknameTest.kt
+++ android/TodokTodok/domain/src/test/java/com/example/domain/model/NicknameTest.kt
@@ -2,14 +2,52 @@ package com.example.domain.model
 
 import com.example.domain.model.member.NickNameException
 import com.example.domain.model.member.Nickname
-import org.assertj.core.api.Assertions.assertThatThrownBy
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class NicknameTest {
-    @ParameterizedTest
-    @ValueSource(strings = [" ", "  ", " 페토", " 동전 ", "모 찌"])
-    fun `닉네임은 공백 금지다`(value: String) {
-        assertThatThrownBy { Nickname(value) }.isInstanceOf(NickNameException.InvalidWhiteSpace::class.java)
+    @Test
+    fun `정상적인 닉네임은 예외 없이 생성된다`() {
+        assertDoesNotThrow { Nickname("홍길동") }
+        assertDoesNotThrow { Nickname("gildong1") }
+        assertDoesNotThrow { Nickname("길1a") }
+    }
+
+    @Test
+    fun `닉네임에 공백이 포함되면 InvalidWhiteSpace 예외 발생한다`() {
+        val exception =
+            assertThrows<NickNameException.InvalidWhiteSpace> {
+                Nickname("홍 길동")
+            }
+        assertEquals(NickNameException.InvalidWhiteSpace, exception)
+    }
+
+    @Test
+    fun `닉네임에 특수문자가 포함되면 InvalidCharacters 예외 발생한다`() {
+        val exception =
+            assertThrows<NickNameException.InvalidCharacters> {
+                Nickname("홍길동!")
+            }
+        assertEquals(NickNameException.InvalidCharacters, exception)
+    }
+
+    @Test
+    fun `닉네임이 비어있으면 InvalidLength 예외 발생한다`() {
+        val exception =
+            assertThrows<NickNameException.InvalidLength> {
+                Nickname("")
+            }
+        assertEquals(NickNameException.InvalidLength, exception)
+    }
+
+    @Test
+    fun `닉네임이 8글자를 초과하면 InvalidLength 예외 발생한다`() {
+        val exception =
+            assertThrows<NickNameException.InvalidLength> {
+                Nickname("가나다라마바사아자차")
+            }
+        assertEquals(NickNameException.InvalidLength, exception)
     }
 }
