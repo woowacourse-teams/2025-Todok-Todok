36abe3752a0a8d17eb862746628798a0e21e41d4
Chaeyoung714 <202102760@hufs.ac.kr>
2025-07-24 10:00:13 +0900

    refactor: 스웨거 추가

--- backend/src/main/java/todoktodok/backend/note/presentation/NoteController.java
+++ backend/src/main/java/todoktodok/backend/note/presentation/NoteController.java
@@ -41,7 +41,7 @@ public class NoteController {
                 .build();
     }
 
-    @Operation(summary = "내 기록 조회 API")
+    @Operation(summary = "도서별 내 기록 조회")
     @Auth(value = Role.USER)
     @GetMapping("/mine")
     public ResponseEntity<List<MyNoteResponse>> getMyNotes(
@@ -52,6 +52,7 @@ public class NoteController {
                 .body(noteQueryService.getMyNotes(memberId, bookId));
     }
 
+    @Operation(summary = "기록 단일 조회")
     @Auth(value = Role.USER)
     @GetMapping("/{noteId}")
     public ResponseEntity<MyNoteResponse> getNoteById(
