5974b881f91e19eb91f1d3022f2f4f61f9ec47a6
chanho0908 <uni070@naver.com>
2025-07-25 13:54:12 +0900

    refactor: #138 `lifecycle-livedata-ktx` 의존성 번들로 통합

--- android/TodokTodok/app/build.gradle.kts
+++ android/TodokTodok/app/build.gradle.kts
@@ -89,7 +89,7 @@ dependencies {
     implementation(libs.bundles.network)
     implementation(libs.bundles.google)
     implementation(libs.bundles.glide)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
+
     testImplementation(libs.bundles.test)
     testImplementation(libs.androidx.core.testing)
 
