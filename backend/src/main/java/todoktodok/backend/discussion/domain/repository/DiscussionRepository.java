426a213b5820a4fcf64ffea5d6a416a8f2580723
Chaeyoung714 <202102760@hufs.ac.kr>
2025-07-28 20:30:14 +0900

    feat: 토론방 검색 API 추가

--- backend/src/main/java/todoktodok/backend/discussion/domain/repository/DiscussionRepository.java
+++ backend/src/main/java/todoktodok/backend/discussion/domain/repository/DiscussionRepository.java
@@ -1,7 +1,48 @@
 package todoktodok.backend.discussion.domain.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import todoktodok.backend.discussion.domain.Discussion;
+import todoktodok.backend.member.domain.Member;
 
 public interface DiscussionRepository extends JpaRepository<Discussion, Long> {
+
+    List<Discussion> findDiscussionsByMember(Member member);
+
+    List<Discussion> findDiscussionsByTitleContainingIgnoreCaseOrBook_TitleIsContainingIgnoreCase(String title, String bookTitle);
+
+    @Query("""
+        select d from Discussion d
+        where upper(d.title) like upper(concat('%', :keyword, '%'))
+        and d.deletedAt is null
+        union
+        select d from Discussion d
+        join d.book b
+        where upper(b.title) like upper(concat('%', :keyword, '%'))
+        and d.deletedAt is null
+        and b.deletedAt is null
+    """)
+    List<Discussion> searchByKeyword(
+            @Param("keyword") String keyword
+    );
+
+    @Query("""
+        select d from Discussion d
+        where upper(d.title) like upper(concat('%', :keyword, '%'))
+        and d.deletedAt is null
+        and d.member = :member
+        union
+        select d from Discussion d
+        join d.book b
+        where upper(b.title) like upper(concat('%', :keyword, '%'))
+        and d.deletedAt is null
+        and b.deletedAt is null
+        and d.member = :member
+    """)
+    List<Discussion> searchByKeywordAndMember(
+            @Param("keyword") String keyword,
+            @Param("member") Member member
+    );
 }
--- backend/src/main/java/todoktodok/backend/discussion/presentation/DiscussionControllerV2.java
+++ backend/src/main/java/todoktodok/backend/discussion/presentation/DiscussionControllerV2.java
@@ -5,16 +5,21 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.net.URI;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import todoktodok.backend.discussion.application.dto.request.DiscussionRequestV2;
+import todoktodok.backend.discussion.application.dto.response.DiscussionResponse;
 import todoktodok.backend.discussion.application.service.command.DiscussionCommandService;
+import todoktodok.backend.discussion.application.service.query.DiscussionQueryService;
+import todoktodok.backend.discussion.domain.DiscussionFilterType;
 import todoktodok.backend.global.auth.Auth;
 import todoktodok.backend.global.auth.Role;
 import todoktodok.backend.global.resolver.LoginMember;
@@ -26,6 +31,7 @@ import todoktodok.backend.global.resolver.LoginMember;
 public class DiscussionControllerV2 {
 
     private final DiscussionCommandService discussionCommandService;
+    private final DiscussionQueryService discussionQueryService;
 
     @Operation(summary = "토론방 생성 API")
     @Auth(value = Role.USER)
@@ -40,4 +46,17 @@ public class DiscussionControllerV2 {
                 .location(URI.create("/api/v1/discussions/" + discussionId))
                 .build();
     }
+
+    @Operation(summary = "토론방 필터 API")
+    @Auth(value = Role.USER)
+    @GetMapping
+    public ResponseEntity<List<DiscussionResponse>> getDiscussionsByKeywordAndType(
+            @Parameter(hidden = true) @LoginMember final Long memberId,
+            @RequestParam(required = false) final String keyword,
+            @RequestParam final DiscussionFilterType type
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(discussionQueryService.getDiscussionsByKeywordAndType(memberId, keyword, type));
+    }
+
 }
