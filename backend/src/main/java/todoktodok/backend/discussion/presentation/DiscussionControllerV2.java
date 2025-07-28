6dd8d31f2fc92871e34ed4c8c77bfa728ec05c95
ljhee92 <weilainib7@gmail.com>
2025-07-28 16:46:29 +0900

    feat: 토론방 생성 API v1 -> v2 수정
    - request 기록id -> 도서id 수정

---
+++ backend/src/main/java/todoktodok/backend/discussion/presentation/DiscussionControllerV2.java
@@ -0,0 +1,50 @@
+package todoktodok.backend.discussion.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import java.net.URI;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import todoktodok.backend.discussion.application.dto.request.DiscussionRequestV2;
+import todoktodok.backend.discussion.application.service.command.DiscussionCommandService;
+import todoktodok.backend.global.auth.Auth;
+import todoktodok.backend.global.auth.Role;
+import todoktodok.backend.global.resolver.LoginMember;
+
+@Tag(name = "discussion-controller")
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v2/discussions")
+public class DiscussionControllerV2 {
+
+    private final DiscussionCommandService discussionCommandService;
+
+    @Operation(summary = "토론방 생성 API")
+    @Auth(value = Role.USER)
+    @PostMapping
+    public ResponseEntity<Void> createDiscussion(
+            @Parameter(hidden = true) @LoginMember final Long memberId,
+            @RequestBody @Valid final DiscussionRequestV2 discussionRequestV2
+    ) {
+        final Long discussionId = discussionCommandService.createDiscussionV2(memberId, discussionRequestV2);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .location(createUri(discussionId))
+                .build();
+    }
+
+    private URI createUri(final Long id) {
+        return ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+    }
+}
