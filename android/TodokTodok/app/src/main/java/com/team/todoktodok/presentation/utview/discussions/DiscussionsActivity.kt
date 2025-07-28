0163c75d1c921f3bc3e7448c5a580372faf01efd
chanho0908 <uni070@naver.com>
2025-07-28 18:27:38 +0900

    feat: #148 검색창에 포커스시 힌트가 사라지는 기능 구현

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/utview/discussions/DiscussionsActivity.kt
@@ -0,0 +1,35 @@
+package com.team.todoktodok.presentation.utview.discussions
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.team.todoktodok.R
+import com.team.todoktodok.databinding.ActivityDiscussionsBinding
+import com.team.todoktodok.presentation.core.ext.clearHintOnFocus
+
+class DiscussionsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDiscussionsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDiscussionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpSystemBars()
+        initView(binding)
+    }
+
+    private fun setUpSystemBars() {
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun initView(binding: ActivityDiscussionsBinding) {
+        with(binding) {
+            val hint = getString(R.string.ut_discussion_search_bar_hint)
+            etSearchDiscussion.clearHintOnFocus(binding.etSearchDiscussionLayout, hint)
