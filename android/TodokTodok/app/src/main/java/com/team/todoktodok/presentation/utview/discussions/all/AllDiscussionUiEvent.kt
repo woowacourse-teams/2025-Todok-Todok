890d192f26a5c0dc46f95bf042745fda35bdb1ba
chanho0908 <uni070@naver.com>
2025-07-29 17:55:01 +0900

    refactor: #170 전체 토론방 화면에 대한 ViewModel, UiEvent 제거

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/utview/discussions/all/AllDiscussionUiEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.team.todoktodok.presentation.utview.discussions.all
-
-sealed interface AllDiscussionUiEvent {
-    data class NavigateToDetail(
-        val id: Long,
-    ) : AllDiscussionUiEvent
-}
