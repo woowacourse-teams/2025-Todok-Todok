0c586114660a11554cfa2f6808c5712ad9e5d63d
chanho0908 <uni070@naver.com>
2025-07-29 22:36:17 +0900

    feat: #170 DiscussionsUiState에 검색 키워드 필드 추가

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/utview/discussions/DiscussionsUiState.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/utview/discussions/DiscussionsUiState.kt
@@ -6,5 +6,6 @@ import com.team.domain.model.DiscussionFilter
 data class DiscussionsUiState(
     val allDiscussions: List<Discussion> = emptyList(),
     val myDiscussions: List<Discussion> = emptyList(),
+    val searchKeyword: String = "",
     val tab: DiscussionFilter = DiscussionFilter.ALL,
 )
