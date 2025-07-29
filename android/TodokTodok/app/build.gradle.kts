2fa44fd8bcd7e29dfe92e27f87781ff544317719
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-07-29 13:06:27 +0900

    feat: 토론방 생성 구현

--- android/TodokTodok/app/build.gradle.kts
+++ android/TodokTodok/app/build.gradle.kts
@@ -89,6 +89,8 @@ dependencies {
     implementation(libs.bundles.network)
     implementation(libs.bundles.google)
     implementation(libs.bundles.glide)
+    implementation(libs.activity)
+    implementation(libs.androidx.constraintlayout)
 
     testImplementation(libs.bundles.test)
     testImplementation(libs.androidx.core.testing)
