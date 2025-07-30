376a02af3b1eeefba9ae67fa12065ade88c77791
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-07-30 12:19:56 +0900

    test: unit test 통과를 위한 임시 함수 생성

--- android/TodokTodok/app/src/test/java/com/team/todoktodok/fake/datasource/FakeDiscussionRemoteDataSource.kt
+++ android/TodokTodok/app/src/test/java/com/team/todoktodok/fake/datasource/FakeDiscussionRemoteDataSource.kt
@@ -3,6 +3,7 @@ package com.team.todoktodok.fake.datasource
 import com.team.domain.model.DiscussionFilter
 import com.team.todoktodok.data.datasource.discussion.DiscussionRemoteDataSource
 import com.team.todoktodok.data.network.request.DiscussionRequest
+import com.team.todoktodok.data.network.request.DiscussionRoomRequest
 import com.team.todoktodok.data.network.response.discussion.BookResponse
 import com.team.todoktodok.data.network.response.discussion.DiscussionResponse
 import com.team.todoktodok.data.network.response.discussion.MemberResponse
@@ -112,4 +113,7 @@ class FakeDiscussionRemoteDataSource : DiscussionRemoteDataSource {
         }
 
     override suspend fun saveDiscussion(discussionRequest: DiscussionRequest): Long = 0
+    override suspend fun saveDiscussionRoom(discussionRequest: DiscussionRoomRequest) {
+        TODO("Not yet implemented")
+    }
 }
