adf2ba57a20b3f8d878e36d74967f8e2f1dee3c6
chanho0908 <uni070@naver.com>
2025-08-03 13:31:21 +0900

    feat: #203 유저 프로필 화면 상단 툴바 뷰홀더 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/profile/viewholder/UserProfileHeaderViewHolder.kt
@@ -0,0 +1,40 @@
+package com.team.todoktodok.presentation.view.profile.viewholder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.team.todoktodok.databinding.ItemUserHeaderBinding
+
+class UserProfileHeaderViewHolder private constructor(
+    binding: ItemUserHeaderBinding,
+    private val handler: Handler,
+) : RecyclerView.ViewHolder(binding.root) {
+    init {
+        with(binding) {
+            ivLogo.setOnClickListener {
+                handler.onClickLogo()
+            }
+
+            ivSetting.setOnClickListener {
+                handler.onClickSetting()
+            }
+        }
+    }
+
+    companion object {
+        fun UserProfileHeaderViewHolder(
+            parent: ViewGroup,
+            handler: Handler,
+        ): UserProfileHeaderViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemUserHeaderBinding.inflate(inflater, parent, false)
+            return UserProfileHeaderViewHolder(binding, handler)
+        }
+    }
+
+    interface Handler {
+        fun onClickSetting()
+
+        fun onClickLogo()
+    }
+}
