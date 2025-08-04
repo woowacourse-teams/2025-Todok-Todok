ecfb279e6184e1a0b2e33a938196e44bb1249d02
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-04 23:19:56 +0900

    feat: #197 BookRepository 구현

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/data/di/RepositoryModule.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/di/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package com.team.todoktodok.data.di
 
+import com.team.domain.repository.BookRepository
 import com.team.domain.repository.CommentRepository
 import com.team.domain.repository.DiscussionRepository
 import com.team.domain.repository.MemberRepository
+import com.team.todoktodok.data.repository.DefaultBookRepository
 import com.team.todoktodok.data.repository.DefaultCommentRepository
 import com.team.todoktodok.data.repository.DefaultDiscussionRepository
 import com.team.todoktodok.data.repository.DefaultMemberRepository
@@ -19,4 +21,6 @@ class RepositoryModule(
     val commentRepository: CommentRepository by lazy { DefaultCommentRepository(dataSourceModule.commentRemoteDataSource) }
 
     val memberRepository: MemberRepository by lazy { DefaultMemberRepository(dataSourceModule.memberRemoteDataSource) }
+
+    val bookRepository: BookRepository by lazy { DefaultBookRepository(dataSourceModule.bookRemoteDataSource) }
 }
