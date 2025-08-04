267a35ed135575b7703adfcf84e3b318d4a557c6
윤동현 <donghyunyoon81@gmail.com>
2025-08-04 15:36:15 +0900

    feat: #216 토론 상세 화면 댓글 기능 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/discussiondetail/comments/vm/CommentsViewModel.kt
@@ -0,0 +1,51 @@
+package com.team.todoktodok.presentation.view.discussiondetail.comments.vm
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.team.domain.model.Comment
+import com.team.domain.repository.CommentRepository
+import com.team.todoktodok.presentation.core.event.MutableSingleLiveData
+import com.team.todoktodok.presentation.core.event.SingleLiveData
+import com.team.todoktodok.presentation.view.discussiondetail.comments.CommentsUiEvent
+import kotlinx.coroutines.launch
+
+class CommentsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val commentRepository: CommentRepository,
+) : ViewModel() {
+    private val discussionId =
+        savedStateHandle.get<Long>(KEY_DISCUSSION_ID) ?: throw IllegalStateException()
+
+    private val _comments = MutableLiveData<List<Comment>>()
+    val comments: LiveData<List<Comment>> = _comments
+
+    private val _uiEvent = MutableSingleLiveData<CommentsUiEvent>()
+    val uiEvent: SingleLiveData<CommentsUiEvent> = _uiEvent
+
+    init {
+        viewModelScope.launch {
+            loadComments()
+        }
+    }
+
+    fun commentsReload() {
+        viewModelScope.launch {
+            loadComments()
+        }
+    }
+
+    fun showCommentCreate() {
+        _uiEvent.setValue(CommentsUiEvent.ShowCommentCreate(discussionId))
+    }
+
+    private suspend fun loadComments() {
+        _comments.value = commentRepository.getCommentsByDiscussionRoomId(discussionId)
+    }
+
+    companion object {
+        const val KEY_DISCUSSION_ID = "discussionId"
+    }
+}
