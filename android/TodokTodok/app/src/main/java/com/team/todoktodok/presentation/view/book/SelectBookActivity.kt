b654492c1a54605575c2cdab422d8d765844c738
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-04 23:16:49 +0900

    feat: #197 책 선택 화면과 어댑터 구현

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/SelectBookActivity.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/SelectBookActivity.kt
@@ -1,21 +1,132 @@
 package com.team.todoktodok.presentation.view.book
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.team.todoktodok.R
+import com.team.todoktodok.App
+import com.team.todoktodok.databinding.ActivitySelectBookBinding
+import com.team.todoktodok.presentation.view.book.adapter.SearchBooksAdapter
+import com.team.todoktodok.presentation.view.book.vm.SelectBookViewModel
+import com.team.todoktodok.presentation.view.book.vm.SelectBookViewModelFactory
 
 class SelectBookActivity : AppCompatActivity() {
+    private val binding by lazy { ActivitySelectBookBinding.inflate(layoutInflater) }
+
+    private val adapter by lazy {
+        SearchBooksAdapter { position -> viewModel.updateSelectedBook(position) }
+    }
+
+    private val viewModel by viewModels<SelectBookViewModel> {
+        val repositoryModule = (application as App).container.repositoryModule
+        SelectBookViewModelFactory(repositoryModule.bookRepository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initSystemBar()
+        initView()
+        setupUiEvent()
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+            hideKeyBoard(view = currentFocus ?: View(this))
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    private fun initSystemBar() {
         enableEdgeToEdge()
-        setContentView(R.layout.activity_select_book)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.select_book)) { v, insets ->
+        setContentView(binding.main)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                binding.main.paddingLeft,
+                systemBars.top,
+                binding.main.paddingRight,
+                systemBars.bottom,
+            )
             insets
         }
     }
+
+    private fun initView() {
+        binding.apply {
+            rvSearchedBooks.adapter = adapter
+            btnBack.setOnClickListener {
+                viewModel.navigateToBack()
+            }
+            etlSearchKeyword.setEndIconOnClickListener {
+                binding.etSearchKeyword.text = null
+                viewModel.onDeleteKeywordButtonClicked()
+            }
+            etSearchKeyword.setOnEditorActionListener { view, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_DONE) {
+                    val keyword = view.text.toString()
+                    viewModel.onSearchAction(keyword)
+                    true
+                } else {
+                    false
+                }
+            }
+        }
+    }
+
+    private fun setupUiEvent() {
+        viewModel.uiEvent.observe(this) { event ->
+            when (event) {
+                is SelectBookUiEvent.StartLoading -> {
+                    // 로딩 중 스페너 넣기
+                }
+
+                is SelectBookUiEvent.FinishLoading -> {
+                    // 로딩 중 스페너 치우기
+                }
+
+                is SelectBookUiEvent.NavigateToBack -> {
+                    finish()
+                }
+
+                is SelectBookUiEvent.NavigateToCreateDiscussionRoom -> {
+                    // 토론방 생성 페이지로 이동
+                }
+
+                is SelectBookUiEvent.HideKeyboard -> {
+                    binding.etSearchKeyword.clearFocus()
+                    hideKeyBoard(binding.etSearchKeyword)
+                }
+
+                is SelectBookUiEvent.RevealKeyboard -> {
+                    binding.etSearchKeyword.requestFocus()
+                }
+
+                is SelectBookUiEvent.ShowDialog -> {
+                    Toast.makeText(this, getString(event.message), Toast.LENGTH_LONG).show()
+                }
+
+                is SelectBookUiEvent.ShowSearchResult -> {
+                    adapter.submitList(event.books.items)
+                }
+            }
+        }
+    }
+
+    private fun hideKeyBoard(view: View) {
+        val inputMethodManager: InputMethodManager =
+            this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
+
+
+
---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/adapter/SearchBooksViewHolder.kt
@@ -0,0 +1,42 @@
+package com.team.todoktodok.presentation.view.book.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.team.domain.model.Book
+import com.team.todoktodok.databinding.ItemBookBinding
+import com.team.todoktodok.presentation.view.book.loadImage
+
+class SearchBooksViewHolder private constructor(
+    private val binding: ItemBookBinding,
+    private val selectBook: SelectBookClickListener,
+) : RecyclerView.ViewHolder(binding.book) {
+    init {
+        binding.root.setOnClickListener {
+            selectBook.onClick(absoluteAdapterPosition)
+        }
+    }
+
+    fun bind(book: Book) {
+        binding.apply {
+            tvBookTitle.text = book.title
+            tvBookAuthor.text = book.author
+            ivBookImage.loadImage(book.image)
+        }
+    }
+
+    companion object {
+        fun SearchBooksViewHolder(
+            parent: ViewGroup,
+            selectBook: SelectBookClickListener,
+        ): SearchBooksViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemBookBinding.inflate(inflater, parent, false)
+            return SearchBooksViewHolder(binding, selectBook)
+        }
+    }
+
+    fun interface SelectBookClickListener {
+        fun onClick(position: Int)
+    }
+}
