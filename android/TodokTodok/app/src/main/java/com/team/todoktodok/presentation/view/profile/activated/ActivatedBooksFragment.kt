25803675f4e244fd57d5e3f67632a8e3ece96fba
chanho0908 <uni070@naver.com>
2025-08-04 15:05:45 +0900

    feat: #203 프로필 활성화된 책 탭 UI 구성

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/profile/activated/ActivatedBooksFragment.kt
@@ -0,0 +1,44 @@
+package com.team.todoktodok.presentation.view.profile.activated
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.team.todoktodok.R
+import com.team.todoktodok.databinding.FragmentActivatedBooksBinding
+import com.team.todoktodok.presentation.view.profile.activated.vm.ActivatedBooksViewModel
+import com.team.todoktodok.presentation.view.profile.activated.vm.ActivatedBooksViewModelFactory
+
+class ActivatedBooksFragment : Fragment(R.layout.fragment_activated_books) {
+    private val viewModel: ActivatedBooksViewModel by viewModels {
+        ActivatedBooksViewModelFactory()
+    }
+    private lateinit var booksAdapter: BooksAdapter
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        val binding = FragmentActivatedBooksBinding.bind(view)
+
+        initView(binding)
+        setUpUiState()
+    }
+
+    private fun initView(binding: FragmentActivatedBooksBinding) {
+        booksAdapter = BooksAdapter(bookAdapterHandler)
+        binding.rvBooks.adapter = booksAdapter
+    }
+
+    private fun setUpUiState() {
+        viewModel.books.observe(viewLifecycleOwner) { value ->
+            booksAdapter.submitList(value.items)
+        }
+    }
+
+    val bookAdapterHandler =
+        object : BooksAdapter.Handler {
+            override fun onSelectBook(index: Int) {
+            }
+        }
+}
---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/profile/activated/BooksViewHolder.kt
@@ -0,0 +1,59 @@
+package com.team.todoktodok.presentation.view.profile.activated
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.team.domain.model.Book
+import com.team.todoktodok.R
+import com.team.todoktodok.databinding.ItemActivatedBookBinding
+
+class BooksViewHolder private constructor(
+    private val binding: ItemActivatedBookBinding,
+    handler: Handler,
+) : RecyclerView.ViewHolder(binding.root) {
+    init {
+        handler.onSelectBook(absoluteAdapterPosition)
+    }
+
+    fun bind(item: Book) {
+        adjustItemSize()
+
+        Glide
+            .with(binding.root)
+            .load("https://image.aladin.co.kr/product/33413/26/coversum/k582938339_1.jpg")
+            .into(binding.ivBook)
+    }
+
+    private fun adjustItemSize() {
+        val resource = itemView.context.resources
+        val displayMetrics = resource.displayMetrics
+        val screenWidth = displayMetrics.widthPixels
+
+        val spanCount = 3
+        val spacing = resource.getDimensionPixelSize(R.dimen.space_12)
+        val totalSpacing = spacing * (spanCount + 1)
+        val itemWidth = (screenWidth - totalSpacing) / spanCount
+
+        itemView.layoutParams =
+            itemView.layoutParams.apply {
+                width = itemWidth
+                height = (itemWidth * 1.2).toInt()
+            }
+    }
+
+    companion object {
+        fun BooksViewHolder(
+            parent: ViewGroup,
+            handler: Handler,
+        ): BooksViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemActivatedBookBinding.inflate(inflater, parent, false)
+            return BooksViewHolder(binding, handler)
+        }
+    }
+
+    fun interface Handler {
+        fun onSelectBook(index: Int)
+    }
+}
