f7b94561bf804df03ec2a6a9a4aaff9444a97701
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-04 23:17:49 +0900

    feat: #197 토론방 도서 선택 화면 ViewModel, UiState, UiEvent 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/SelectBookUiEvent.kt
@@ -0,0 +1,29 @@
+package com.team.todoktodok.presentation.view.book
+
+import android.view.View
+import com.team.domain.model.Book
+import com.team.domain.model.Books
+
+sealed class SelectBookUiEvent {
+    data object StartLoading : SelectBookUiEvent()
+
+    data object FinishLoading : SelectBookUiEvent()
+
+    data object HideKeyboard : SelectBookUiEvent()
+
+    data object RevealKeyboard : SelectBookUiEvent()
+
+    data object NavigateToBack : SelectBookUiEvent()
+
+    data class NavigateToCreateDiscussionRoom(
+        val book: Book,
+    ) : SelectBookUiEvent()
+
+    data class ShowDialog(
+        val message: Int,
+    ) : SelectBookUiEvent()
+
+    data class ShowSearchResult(
+        val books: Books,
+    ) : SelectBookUiEvent()
+}
