5c2c132179c379b1205a86ab7c5d49fe5d7d282a
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-05 22:11:05 +0900

    refactor: #238 책 검색 API 응답 타입 변경 및 도메인 모델 매핑 로직 이동

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/data/datasource/book/BookRemoteDataSource.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/datasource/book/BookRemoteDataSource.kt
@@ -1,7 +1,7 @@
 package com.team.todoktodok.data.datasource.book
 
-import com.team.domain.model.Books
+import com.team.todoktodok.data.network.response.discussion.BookResponse
 
 interface BookRemoteDataSource {
-    suspend fun fetchBooks(keyword: String): Books
+    suspend fun fetchBooks(keyword: String): List<BookResponse>
 }
--- android/TodokTodok/app/src/main/java/com/team/todoktodok/data/repository/DefaultBookRepository.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/repository/DefaultBookRepository.kt
@@ -3,9 +3,17 @@ package com.team.todoktodok.data.repository
 import com.team.domain.model.Books
 import com.team.domain.repository.BookRepository
 import com.team.todoktodok.data.datasource.book.BookRemoteDataSource
+import com.team.todoktodok.data.network.response.discussion.BookResponse
+import com.team.todoktodok.data.network.response.discussion.toDomain
 
 class DefaultBookRepository(
     private val bookRemoteDataSource: BookRemoteDataSource,
 ) : BookRepository {
-    override suspend fun fetchBooks(keyword: String): Books = bookRemoteDataSource.fetchBooks(keyword)
+    override suspend fun fetchBooks(keyword: String): Books {
+        val value =
+            bookRemoteDataSource
+                .fetchBooks(keyword)
+                .map { bookResponse: BookResponse -> bookResponse.toDomain() }
+        return Books(value)
+    }
 }
