43c22bb2777b4d932a245855e3e0a9e41d267ba6
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-06 12:08:31 +0900

    refactor: #277 사용하지 않는 import 제거

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/data/network/service/DiscussionService.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/network/service/DiscussionService.kt
@@ -1,12 +1,7 @@
 package com.team.todoktodok.data.network.service
 
-import com.team.todoktodok.data.network.request.DiscussionRequest
-import com.team.todoktodok.data.network.request.DiscussionRoomRequest
 import com.team.todoktodok.data.network.response.discussion.DiscussionResponse
-import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
