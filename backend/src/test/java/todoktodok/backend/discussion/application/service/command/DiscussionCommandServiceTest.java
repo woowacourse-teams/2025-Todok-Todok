34de2511574a4c701ab2ce1c33ff3bd1d86ccaf0
sonjh919 <kingjh1125@gmail.com>
2025-08-06 17:41:07 +0900

    refactor: 컨벤션 수정

--- backend/src/test/java/todoktodok/backend/discussion/application/service/command/DiscussionCommandServiceTest.java
+++ backend/src/test/java/todoktodok/backend/discussion/application/service/command/DiscussionCommandServiceTest.java
@@ -31,8 +31,6 @@ class DiscussionCommandServiceTest {
     @Autowired
     private DiscussionCommandService discussionCommandService;
 
-
-
     @BeforeEach
     void setUp() {
         databaseInitializer.clear();
