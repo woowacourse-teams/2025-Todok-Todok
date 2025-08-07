7af9427de8f6636f790dc8658791bba4806906db
chanho0908 <uni070@naver.com>
2025-08-07 19:06:22 +0900

    feat: #302 프로필 설정 페이지 화면 구성
    - 설정 메뉴에 따른 화면 전환 로직 구현
    - 프로필 설정, 차단 사용자 관리, 프로필 수정 화면 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/setting/SettingFragment.kt
@@ -0,0 +1,37 @@
+package com.team.todoktodok.presentation.view.setting
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.team.todoktodok.App
+import com.team.todoktodok.R
+import com.team.todoktodok.databinding.FragmentSettingBinding
+import com.team.todoktodok.presentation.view.setting.vm.SettingViewModel
+import com.team.todoktodok.presentation.view.setting.vm.SettingViewModelFactory
+import kotlin.getValue
+
+class SettingFragment : Fragment(R.layout.fragment_setting) {
+    private val viewModel: SettingViewModel by activityViewModels { SettingViewModelFactory() }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentSettingBinding.bind(view)
+        initView(binding)
+    }
+
+    private fun initView(binding: FragmentSettingBinding) {
+        with(binding) {
+            llModifyProfile.setOnClickListener {
+                viewModel.changeScreen(SettingScreen.MODIFY_PROFILE)
+            }
+
+            llBlockMembers.setOnClickListener {
+                viewModel.changeScreen(SettingScreen.MANAGE_BLOCKED_USERS)
+            }
+        }
+    }
+}
