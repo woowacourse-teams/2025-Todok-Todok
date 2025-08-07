336b70d7f25a2fac39a4680f0578b1be08a72b19
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-07 15:18:28 +0900

    feat: #293 토론방 생성 화면 구현

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/discussion/create/CreateDiscussionRoomActivity.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/discussion/create/CreateDiscussionRoomActivity.kt
@@ -3,34 +3,106 @@ package com.team.todoktodok.presentation.view.discussion.create
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.team.domain.model.Book
 import com.team.todoktodok.App
+import com.team.todoktodok.R
 import com.team.todoktodok.databinding.ActivityCreateDiscussionRoomBinding
 import com.team.todoktodok.presentation.core.ext.getParcelableCompat
+import com.team.todoktodok.presentation.core.ext.loadImage
+import com.team.todoktodok.presentation.view.book.SelectBookActivity
 import com.team.todoktodok.presentation.view.discussion.create.vm.CreateDiscussionRoomViewModel
 import com.team.todoktodok.presentation.view.discussion.create.vm.CreateDiscussionRoomViewModelFactory
+import com.team.todoktodok.presentation.view.discussiondetail.DiscussionDetailActivity
 import com.team.todoktodok.presentation.view.serialization.SerializationBook
 
 class CreateDiscussionRoomActivity : AppCompatActivity() {
     private val binding by lazy { ActivityCreateDiscussionRoomBinding.inflate(layoutInflater) }
 
-    private val viewModel by viewModels<CreateDiscussionRoomViewModel>  {
-        val repositoryModule = (application as  App).container.repositoryModule
-        CreateDiscussionRoomViewModelFactory(repositoryModule.bookRepository, repositoryModule.discussionRepository)
+    private val viewModel by viewModels<CreateDiscussionRoomViewModel> {
+        val repositoryModule = (application as App).container.repositoryModule
+        CreateDiscussionRoomViewModelFactory(
+            repositoryModule.bookRepository,
+            repositoryModule.discussionRepository
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initSystemBar()
-
+        setupUi()
         val intent = intent
         val book = intent.getParcelableCompat<SerializationBook>(EXTRA_SELECTED_BOOK).toDomain()
+
+        binding.apply {
+            btnBack.setOnClickListener { finish() }
+            btnEdit.setOnClickListener {
+                val intent = SelectBookActivity.Intent(this@CreateDiscussionRoomActivity)
+                startActivity(intent)
+                finish()
+            }
+            btnCreate.setOnClickListener {
+                viewModel.createDiscussionRoom()
+            }
+
+            etDiscussionRoomTitle.setOnEditorActionListener { view, actionId, event ->
+                handleTextInput(view, actionId)
+            }
+            etDiscussionRoomOpinion.setOnEditorActionListener { view, actionId, event ->
+                handleTextInput(view, actionId)
+            }
+            if (etDiscussionRoomTitle.text.isNullOrBlank() && etDiscussionRoomOpinion.text.isNullOrBlank()) {
+                btnCreate.isEnabled = false
+            } else {
+                btnCreate.isEnabled = true
+                btnCreate.setTextColor(
+                    ContextCompat.getColor(
+                        this@CreateDiscussionRoomActivity,
+                        R.color.green_1A
+                    )
+                )
+            }
+        }
     }
+
+    private fun setupUi() {
+        viewModel.book.observe(this) { book: Book ->
+            binding.tvBookTitle.text = book.title
+            binding.tvBookAuthor.text = book.author
+            binding.ivBookImage.loadImage(book.image)
+        }
+        viewModel.discussionRoomId.observe(this) { discussionRoomId: Long ->
+            val intent =
+                DiscussionDetailActivity.Intent(this@CreateDiscussionRoomActivity, discussionRoomId)
+            startActivity(intent)
+            finish()
+        }
+
+
+
+    }
+
+    private fun handleTextInput(
+        view: TextView,
+        actionId: Int,
+    ): Boolean =
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            when (view.id) {
+                binding.etDiscussionRoomTitle.id -> viewModel.onTitleChanged(view.text.toString())
+                binding.etDiscussionRoomOpinion.id -> viewModel.onOpinionChanged(view.text.toString())
+            }
+            true
+        } else {
+            false
+        }
 
     private fun initSystemBar() {
         enableEdgeToEdge()
@@ -58,6 +130,7 @@ class CreateDiscussionRoomActivity : AppCompatActivity() {
             when (mode) {
                 is CreateDiscussionRoomMode.Create ->
                     intent.putExtra(EXTRA_SELECTED_BOOK, mode.selectedBook)
+
                 is CreateDiscussionRoomMode.Edit ->
                     intent.putExtra(EXTRA_DISCUSSION_ROOM_ID, mode.discussionRoomId)
             }
--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/discussion/create/vm/CreateDiscussionRoomViewModel.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/discussion/create/vm/CreateDiscussionRoomViewModel.kt
@@ -1,11 +1,72 @@
 package com.team.todoktodok.presentation.view.discussion.create.vm
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.team.domain.model.Book
+import com.team.domain.model.Discussion
 import com.team.domain.repository.BookRepository
 import com.team.domain.repository.DiscussionRepository
+import com.team.todoktodok.data.datasource.token.TokenDataSource
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class CreateDiscussionRoomViewModel(
     private val bookRepository: BookRepository,
     private val discussionRepository: DiscussionRepository,
+    private val tokenDataSource: TokenDataSource,
 ) : ViewModel() {
+    private val _book: MutableLiveData<Book> = MutableLiveData()
+    val book: LiveData<Book> = _book
+
+    private val _title: MutableLiveData<String> = MutableLiveData()
+    val title: LiveData<String> = _title
+
+    private val _opinion: MutableLiveData<String> = MutableLiveData()
+    val opinion: LiveData<String> = _opinion
+
+    private val _discussionRoomId: MutableLiveData<Long> = MutableLiveData()
+    val discussionRoomId: LiveData<Long> = _discussionRoomId
+
+    init {
+        _book.value = Book(
+            id = 1L,
+            title = "클린 코드",
+            author = "로버트 C. 마틴",
+            image = "https://image.aladin.co.kr/product/1950/55/cover500/8960773417_2.jpg",
+        )
+    }
+
+    fun onTitleChanged(title: String) {
+        _title.value = title
+    }
+
+    fun onOpinionChanged(opinion: String) {
+        _opinion.value = opinion
+    }
+
+    fun createDiscussionRoom() {
+        val book = book.value ?: throw IllegalStateException("책 정보가 없습니다.")
+        val title = title.value ?: throw IllegalStateException("제목이 없습니다.")
+        val opinion = opinion.value ?: throw IllegalStateException("내용이 없습니다.")
+        viewModelScope.launch {
+            val bookId = bookRepository.saveBook(book)
+            val discussionId =
+                discussionRepository.saveDiscussionRoom(
+                    bookId = bookId,
+                    discussionTitle = title,
+                    discussionOpinion = opinion
+                )
+            _discussionRoomId.value = discussionId
+            return@launch
+        }
+    }
+
+    fun getDiscussionRoom() {
+        val discussionRoomId = discussionRoomId.value ?: throw IllegalStateException("토론방 정보가 없습니다.")
+        viewModelScope.launch {
+            val result: Result<Discussion> = discussionRepository.getDiscussion(discussionRoomId)
+        }
+    }
 }
