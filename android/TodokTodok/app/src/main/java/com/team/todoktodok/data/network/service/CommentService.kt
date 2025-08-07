75511ad97929fdb555f6f51929c1dd0b13d657ae
윤동현 <donghyunyoon81@gmail.com>
2025-08-07 17:05:37 +0900

    feat: #309 댓글 좋아요/삭제 기능 추가
    - 댓글 좋아요/취소, 삭제 기능 API 연동
    - LikeAction, LikeStatus enum 클래스 추가

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/data/network/service/CommentService.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/network/service/CommentService.kt
@@ -4,7 +4,9 @@ import com.team.todoktodok.data.network.request.CommentRequest
 import com.team.todoktodok.data.network.response.comment.CommentResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -19,4 +21,16 @@ interface CommentService {
         @Path("discussionId") discussionId: Long,
         @Body commentRequest: CommentRequest,
     ): Response<Unit>
+
+    @POST("v1/discussions/{discussionId}/comments/{commentId}/like")
+    suspend fun toggleLike(
+        @Path("discussionId") discussionId: Long,
+        @Path("commentId") commentId: Long,
+    ): Response<Unit>
+
+    @DELETE("v1/discussions/{discussionId}/comments/{commentId}")
+    suspend fun deleteComment(
+        @Path("discussionId") discussionId: Long,
+        @Path("commentId") commentId: Long,
+    )
 }
--- android/TodokTodok/domain/src/main/java/com/team/domain/repository/CommentRepository.kt
+++ android/TodokTodok/domain/src/main/java/com/team/domain/repository/CommentRepository.kt
@@ -1,6 +1,7 @@
 package com.team.domain.repository
 
 import com.team.domain.model.Comment
+import com.team.domain.model.LikeStatus
 
 interface CommentRepository {
     suspend fun getCommentsByDiscussionRoomId(id: Long): List<Comment>
@@ -8,5 +9,15 @@ interface CommentRepository {
     suspend fun saveComment(
         discussionId: Long,
         content: String,
+    )
+
+    suspend fun toggleLike(
+        discussionId: Long,
+        commentId: Long,
+    ): LikeStatus
+
+    suspend fun deleteComment(
+        discussionId: Long,
+        commentId: Long,
     )
 }
