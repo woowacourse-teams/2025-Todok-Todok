2437d565ce1e8d872c81e534383b53e92a9fe957
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-08 10:32:16 +0900

    refactor: #293 Intent 확장함수 생성

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/core/ext/BundleExt.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/core/ext/BundleExt.kt
@@ -49,16 +49,4 @@ inline fun <reified T : Serializable> Bundle.getSerializableCompat(key: String):
     }
 }
 
-inline fun <reified T : Parcelable> Intent.getParcelableCompat(key: String): T {
-    val value =
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            getParcelableExtra(key, T::class.java)
-        } else {
-            @Suppress("DEPRECATION")
-            getParcelableExtra(key) as? T
-        }
 
-    return requireNotNull(value) {
-        "Bundle extra '$key' is missing or not of expected type ${T::class.java.name}"
-    }
-}
