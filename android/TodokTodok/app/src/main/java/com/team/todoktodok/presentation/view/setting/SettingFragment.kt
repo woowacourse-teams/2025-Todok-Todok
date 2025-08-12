b07623bec2351a6c0fa921c77b22be9330c0a7cd
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-12 17:54:26 +0900

    feat: #355 설정에서 인터넷 창으로 이동하는 로직 구현

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/setting/SettingFragment.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/setting/SettingFragment.kt
@@ -1,5 +1,7 @@
 package com.team.todoktodok.presentation.view.setting
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -29,6 +31,11 @@ class SettingFragment : Fragment(R.layout.fragment_setting) {
 
             llBlockMembers.setOnClickListener {
                 viewModel.changeScreen(SettingScreen.MANAGE_BLOCKED_USERS)
+            }
+
+            llSendFeedback.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/"))
+                startActivity(intent)
             }
         }
     }
