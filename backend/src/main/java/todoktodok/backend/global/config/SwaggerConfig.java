438a003cfedfb1355d5de336597c5a84fa6ea63f
ljhee92 <weilainib7@gmail.com>
2025-08-13 12:32:30 +0900

    chore: Swagger API 설정 및 문서 설명 수정

--- backend/src/main/java/todoktodok/backend/global/config/SwaggerConfig.java
+++ backend/src/main/java/todoktodok/backend/global/config/SwaggerConfig.java
@@ -28,8 +28,7 @@ public class SwaggerConfig {
     }
 
     public OpenApiCustomizer buildUnauthenticatedOpenApi() {
-        final Server unauthenticated = new Server();
-        unauthenticated.url("/").setDescription("Swagger for Unauthenticated");
+        final Server unauthenticated = new Server().url("/");
 
         return openApi -> openApi.addServersItem(unauthenticated);
     }
@@ -55,8 +54,28 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI springTodokTodokOpenAPI() {
-        final String title = "Todok-Todok";
-        final String description = "토독토독 프로젝트";
+        final String title = "토독토독";
+        final String description = """
+                ### API 인증 및 테스트 방법
+                
+                API 테스트 전, 회원가입/로그인 인증 방법은 아래와 같습니다.\n
+                (이미 회원가입이 되어 있는 email이라면 **3~5번 과정**을 생략하시면 됩니다.)
+                
+                ---
+                1. 우측 상단 **Select a definition** -> `unauthenticated` 선택
+                2. **로그인 API**에서 email 입력 -> Execute\n
+                   -> 하단 `Response headers`에서 `authorization: Bearer` 뒤 토큰 **모두 복사** (공백 없이!)\n
+                   (복사할 토큰 예시: eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwicm9sZSI6IlVTRVIiLCJleHAiOjE3
+                   NTU2MTgxNjZ9._-0qTNmPyO1m6LnpEAwkGAB92Es0yBwxNBtmsq_VrGk)
+                3. 우측 상단 **Select a definition** -> `authorization` 선택
+                4. 2번에서 복사한 토큰 -> 우측 초록색 자물쇠 **Authorize** 버튼 클릭 -> Value에 붙여넣기 후 Authorize 버튼 클릭
+                5. **회원가입 API**에서 2번에 입력한 email, 사용할 nickname 입력 -> Execute\n
+                   -> 하단 `Response headers`에서 `authorization: Bearer` 뒤 토큰 **모두 복사** (공백 없이!)\n
+                   (복사할 토큰 예시: eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwicm9sZSI6IlVTRVIiLCJleHAiOjE3
+                   NTU2MTgxNjZ9._-0qTNmPyO1m6LnpEAwkGAB92Es0yBwxNBtmsq_VrGk)
+                6. 2번 또는 4-2번에서 복사한 토큰 -> 우측 초록색 자물쇠 **Authorize** 버튼 클릭 -> Value에 붙여넣기 후 Authorize 버튼 클릭
+                7. API 테스트 가능!
+                """;
 
         final Info info = new Info().title(title).description(description).version("1.0.0");
         return new OpenAPI().info(info);
