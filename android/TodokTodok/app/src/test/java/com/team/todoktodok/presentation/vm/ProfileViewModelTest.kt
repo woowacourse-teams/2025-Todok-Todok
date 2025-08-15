4e8cae8008ea860354d244f5cc747e7c0412a1e3
chanho0908 <uni070@naver.com>
2025-08-16 00:28:27 +0900

    test: #362 ProfileViewModel 테스트 코드 개선

--- android/TodokTodok/app/src/test/java/com/team/todoktodok/presentation/vm/ProfileViewModelTest.kt
+++ android/TodokTodok/app/src/test/java/com/team/todoktodok/presentation/vm/ProfileViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.team.todoktodok.presentation.vm
 
 import com.team.domain.model.Support
+import com.team.domain.model.member.MemberDiscussionType
 import com.team.domain.model.member.MemberId
 import com.team.domain.model.member.MemberId.Companion.DEFAULT_MEMBER_ID
 import com.team.domain.model.member.Profile
@@ -27,6 +28,13 @@ class ProfileViewModelTest {
     private lateinit var repository: MemberRepository
     private lateinit var viewModel: ProfileViewModel
 
+    private fun setMockProfileData() {
+        coEvery { repository.getProfile(any()) } returns
+            Profile(1, "페토", "안녕하세요", "")
+        coEvery { repository.getMemberBooks(any()) } returns emptyList()
+        coEvery { repository.getMemberDiscussionRooms(any(), any()) } returns emptyList()
+    }
+
     @BeforeEach
     fun setUp() {
         repository = mockk(relaxed = true)
@@ -36,8 +44,13 @@ class ProfileViewModelTest {
     @Test
     fun `전달받은 멤버 아이디가 존재하면 다른 유저의 프로필 화면으로 초기 상태가 설정된다`() =
         runTest {
+            // given
+            val id = 10L
+            viewModel.setMemberId(id)
+            setMockProfileData()
+
             // when
-            viewModel.initState(10L)
+            viewModel.initState()
 
             // then
             val state = viewModel.uiState.getOrAwaitValue()
@@ -48,8 +61,12 @@ class ProfileViewModelTest {
     @Test
     fun `전달받은 멤버 아이디가 기본값이면 내 프로필 화면으로 초기 상태가 설정된다`() =
         runTest {
+            // given
+            viewModel.setMemberId(DEFAULT_MEMBER_ID)
+            setMockProfileData()
+
             // when
-            viewModel.initState(DEFAULT_MEMBER_ID)
+            viewModel.initState()
 
             // then
             val state = viewModel.uiState.getOrAwaitValue()
@@ -61,17 +78,19 @@ class ProfileViewModelTest {
     fun `사용자의 프로필을 불러온다`() =
         runTest {
             // given
-            val response = Profile(1, "페토", "나나를 좋아하는", "")
+            val profileResponse = Profile(1, "페토", "나나를 좋아하는", "")
+            coEvery { repository.getProfile(any()) } returns profileResponse
+            coEvery { repository.getMemberBooks(any()) } returns emptyList()
+            coEvery { repository.getMemberDiscussionRooms(any(), any()) } returns emptyList()
 
-            coEvery { repository.getProfile(any()) } returns response
+            viewModel.setMemberId(1)
 
             // when
-            viewModel.initState(1)
+            viewModel.initState()
 
             // then
             val expected = (viewModel.uiState.getOrAwaitValue().items[1] as ProfileItems.InformationItem).value
-
-            expected shouldBe response
+            expected shouldBe profileResponse
         }
 
     @Test
@@ -79,7 +98,9 @@ class ProfileViewModelTest {
         runTest {
             // given
             val memberId = 2L
-            viewModel.initState(memberId)
+            viewModel.setMemberId(memberId)
+            setMockProfileData()
+            viewModel.initState()
 
             // when
             viewModel.supportMember(Support.BLOCK)
@@ -93,12 +114,55 @@ class ProfileViewModelTest {
         runTest {
             // given
             val memberId = 2L
-            viewModel.initState(memberId)
+            viewModel.setMemberId(memberId)
+            setMockProfileData()
+            viewModel.initState()
 
             // when
             viewModel.supportMember(Support.REPORT)
 
             // then
             coVerify { repository.supportMember(MemberId.OtherUser(memberId), Support.REPORT) }
+        }
+
+    @Test
+    fun `프로필을 새로고침한다`() =
+        runTest {
+            // given
+            val initialProfile = Profile(1, "페토", "하나코", "")
+            val updatedProfile = Profile(1, "정페토", "나나", "")
+
+            coEvery { repository.getProfile(any()) } returns initialProfile
+            coEvery { repository.getMemberBooks(any()) } returns emptyList()
+            coEvery { repository.getMemberDiscussionRooms(any(), any()) } returns emptyList()
+
+            viewModel.setMemberId(1)
+            viewModel.initState()
+
+            coEvery { repository.getProfile(any()) } returns updatedProfile
+
+            // when
+            viewModel.refreshProfile()
+
+            // then
+            val state = viewModel.uiState.getOrAwaitValue()
+            (state.items[1] as ProfileItems.InformationItem).value shouldBe updatedProfile
+        }
+
+    @Test
+    fun `initState 호출 시 4개의 데이터 로드 메서드가 호출된다`() =
+        runTest {
+            // given
+            viewModel.setMemberId(5)
+            setMockProfileData()
+
+            // when
+            viewModel.initState()
+
+            // then
+            coVerify(exactly = 1) { repository.getProfile(any()) }
+            coVerify(exactly = 1) { repository.getMemberBooks(any()) }
+            coVerify(exactly = 1) { repository.getMemberDiscussionRooms(any(), MemberDiscussionType.PARTICIPATED) }
+            coVerify(exactly = 1) { repository.getMemberDiscussionRooms(any(), MemberDiscussionType.CREATED) }
         }
 }
