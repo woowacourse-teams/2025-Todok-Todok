cbc0fd135777a523ba48c322a1df55f30124da59
chanho0908 <uni070@naver.com>
2025-08-15 20:11:33 +0900

    feat: #396 리소스 없을 때 보여줄 뷰 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/core/component/ResourceNotFoundView.kt
@@ -0,0 +1,34 @@
+package com.team.todoktodok.presentation.core.component
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.core.view.isGone
+import com.team.todoktodok.databinding.ViewResourceNotFoundBinding
+
+class ResourceNotFoundView
+    @JvmOverloads
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0,
+    ) : FrameLayout(context, attrs) {
+        val binding = ViewResourceNotFoundBinding.inflate(LayoutInflater.from(context), this, true)
+
+        fun show(
+            title: String,
+            subtitle: String,
+        ) {
+            if (isGone) visibility = VISIBLE
+
+            with(binding) {
+                tvEmptyResourceTitle.text = title
+                tvEmptyResourceSubtitle.text = subtitle
+            }
+        }
+
+        fun hide() {
+            visibility = INVISIBLE
+        }
+    }
