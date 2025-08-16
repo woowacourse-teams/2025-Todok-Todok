d436a0f0ba7093c710eefae2bb9cc16cb89a9169
chanho0908 <uni070@naver.com>
2025-08-16 09:03:13 +0900

    refactor: #362 ResourceNotFoundView 리소스 없음 화면 가시성 처리 및 부제목 선택적 표시 수정
    - `isVisible` 및 `GONE`을 사용하여 화면 표시 및 숨김 처리
    - 부제목(subtitle)이 null이거나 비어있을 경우 숨김 처리

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/core/component/ResourceNotFoundView.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/core/component/ResourceNotFoundView.kt
@@ -5,30 +5,30 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import com.team.todoktodok.databinding.ViewResourceNotFoundBinding
 
 class ResourceNotFoundView
-    @JvmOverloads
-    constructor(
-        context: Context,
-        attrs: AttributeSet? = null,
-        defStyleAttr: Int = 0,
-    ) : FrameLayout(context, attrs) {
-        val binding = ViewResourceNotFoundBinding.inflate(LayoutInflater.from(context), this, true)
+@JvmOverloads
+constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : FrameLayout(context, attrs, defStyleAttr) {
+    val binding = ViewResourceNotFoundBinding.inflate(LayoutInflater.from(context), this, true)
 
-        fun show(
-            title: String,
-            subtitle: String,
-        ) {
-            if (isGone) visibility = VISIBLE
-
-            with(binding) {
-                tvEmptyResourceTitle.text = title
-                tvEmptyResourceSubtitle.text = subtitle
-            }
-        }
-
-        fun hide() {
-            visibility = INVISIBLE
+    fun show(
+        title: CharSequence,
+        subtitle: CharSequence? = null,
+    ) {
+        if (!isVisible) isVisible = true
+        with(binding) {
+            tvEmptyResourceTitle.text = title
+            tvEmptyResourceSubtitle.isVisible = !subtitle.isNullOrBlank()
         }
     }
+
+    fun hide() {
+        visibility = GONE
+    }
+}
