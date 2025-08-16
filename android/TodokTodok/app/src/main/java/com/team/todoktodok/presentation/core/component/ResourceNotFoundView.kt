b0fdd6c85ffff586a5d91fa354b6f710def24886
chanho0908 <uni070@naver.com>
2025-08-17 00:06:45 +0900

    refactor: #397 리소스 부재시 화면 스타일 수정
    - 이미지 크기 변경
    - 텍스트 스타일 변경
    - subtitle을 null로 전달가능하게 변경
    - 초기 상태 GONE으로 설정

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/core/component/ResourceNotFoundView.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/core/component/ResourceNotFoundView.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
-import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import com.team.todoktodok.databinding.ViewResourceNotFoundBinding
 
 class ResourceNotFoundView
@@ -13,18 +13,18 @@ class ResourceNotFoundView
         context: Context,
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0,
-    ) : FrameLayout(context, attrs) {
+    ) : FrameLayout(context, attrs, defStyleAttr) {
         val binding = ViewResourceNotFoundBinding.inflate(LayoutInflater.from(context), this, true)
 
         fun show(
-            title: String,
-            subtitle: String,
+            title: CharSequence,
+            subtitle: CharSequence? = null,
         ) {
-            if (isGone) visibility = VISIBLE
-
+            if (!isVisible) isVisible = true
             with(binding) {
                 tvEmptyResourceTitle.text = title
                 tvEmptyResourceSubtitle.text = subtitle
+                tvEmptyResourceSubtitle.isVisible = !subtitle.isNullOrBlank()
             }
         }
 
