c0ff68d9187d322a23018c9b3168ce8609e8ab26
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-16 20:01:18 +0900

    refactor: #384 도서 검색 관련 UiEvent 및 ErrorType 명칭 변경

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/ErrorSelectBookType.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/ErrorSelectBookType.kt
@@ -1,8 +1,12 @@
 package com.team.todoktodok.presentation.view.book
 
-enum class ErrorSelectBookType {
-    ERROR_NO_SELECTED_BOOK,
-    ERROR_EMPTY_KEYWORD,
-    ERROR_DELETE_KEYWORD,
-    ERROR_NETWORK,
+import com.team.todoktodok.R
+
+enum class SelectBookErrorType(
+    val id: Int,
+) {
+    ERROR_NO_SELECTED_BOOK(R.string.error_no_selected_book),
+    ERROR_EMPTY_KEYWORD(R.string.error_empty_keyword),
+    ERROR_SAME_KEYWORD(R.string.error_same_keyword),
+    ERROR_NETWORK(R.string.error_network),
 }
