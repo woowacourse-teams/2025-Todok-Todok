094a94f3fd5affa9dd04fdba83d5559d3dfade5e
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-19 12:08:25 +0900

    feat: #446 저장된 파일 불러오는 UiEvent 구현

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/SelectBookUiEvent.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/SelectBookUiEvent.kt
@@ -3,6 +3,8 @@ package com.team.todoktodok.presentation.view.book
 import com.team.domain.model.Book
 
 sealed class SelectBookUiEvent {
+    data object ShowSavedDiscussionRoom : SelectBookUiEvent()
+
     data object HideKeyboard : SelectBookUiEvent()
 
     data class NavigateToCreateDiscussionRoom(
