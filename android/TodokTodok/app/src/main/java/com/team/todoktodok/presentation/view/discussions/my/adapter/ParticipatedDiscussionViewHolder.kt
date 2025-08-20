b03f7cc026312a98bf783e5c35e089b239e12387
chanho0908 <uni070@naver.com>
2025-08-20 15:48:14 +0900

    feat: #489 내가 참여한 토론방 목록 ViewHolder 추가
    - item_my_participated_discussion.xml 추가
    - ParticipatedDiscussionViewHolder.kt 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/discussions/my/adapter/ParticipatedDiscussionViewHolder.kt
@@ -0,0 +1,44 @@
+package com.team.todoktodok.presentation.view.discussions.my.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.team.todoktodok.databinding.ItemMyParticipatedDiscussionBinding
+import com.team.todoktodok.presentation.view.discussions.adapter.DiscussionAdapter
+
+class ParticipatedDiscussionViewHolder private constructor(
+    binding: ItemMyParticipatedDiscussionBinding,
+    private val handler: Handler,
+) : RecyclerView.ViewHolder(binding.root) {
+    private val discussionAdapter = DiscussionAdapter(handler)
+
+    init {
+        with(binding) {
+            rvParticipatedDiscussionHeader.setOnClickListener {
+                handler.onClickMyParticipatedDiscussionHeader()
+            }
+
+            rvParticipatedDiscussion.adapter = discussionAdapter
+            rvParticipatedDiscussion.setHasFixedSize(true)
+        }
+    }
+
+    fun bind(item: MyDiscussionItems.ParticipatedDiscussionItem) {
+        discussionAdapter.submitList(item.items)
+    }
+
+    companion object {
+        fun ParticipatedDiscussionViewHolder(
+            parent: ViewGroup,
+            handler: Handler,
+        ): ParticipatedDiscussionViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemMyParticipatedDiscussionBinding.inflate(inflater, parent, false)
+            return ParticipatedDiscussionViewHolder(binding, handler)
+        }
+    }
+
+    interface Handler : DiscussionAdapter.Handler {
+        fun onClickMyParticipatedDiscussionHeader()
+    }
+}
