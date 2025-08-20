1b674d52b84222a45606b1f211e2f27faa18934e
chanho0908 <uni070@naver.com>
2025-08-21 01:30:40 +0900

    refactor: #504 PageInfo의 EMPTY 상수 정의 및 빈 검색어 상수화

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/discussions/DiscussionsUiState.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/discussions/DiscussionsUiState.kt
@@ -20,8 +20,8 @@ data class DiscussionsUiState(
         ),
     val myDiscussions: List<MyDiscussionItems> = listOf(),
     val latestDiscussions: List<DiscussionUiState> = emptyList(),
-    val latestPage: PageInfo = PageInfo(hasNext = false, nextCursor = ""),
-    val searchKeyword: String = "",
+    val latestPage: PageInfo = PageInfo.EMPTY,
+    val searchKeyword: String = EMPTY_SEARCH_KEYWORD,
     val filter: DiscussionFilter = DiscussionFilter.ALL,
     val isLoading: Boolean = false,
 ) {
@@ -74,6 +74,7 @@ data class DiscussionsUiState(
 
     companion object {
         private const val MY_DISCUSSION_SIZE = 3
+        private const val EMPTY_SEARCH_KEYWORD = ""
     }
 }
 
--- android/TodokTodok/domain/src/main/java/com/team/domain/model/latest/PageInfo.kt
+++ android/TodokTodok/domain/src/main/java/com/team/domain/model/latest/PageInfo.kt
@@ -3,4 +3,8 @@ package com.team.domain.model.latest
 data class PageInfo(
     val hasNext: Boolean,
     val nextCursor: String,
-)
+) {
+    companion object {
+        val EMPTY = PageInfo(false, "")
+    }
+}
