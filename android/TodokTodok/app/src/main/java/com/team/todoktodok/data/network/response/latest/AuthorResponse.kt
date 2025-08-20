61a20619bff7a5d26143c3a6d148ffb9c682baaf
chanho0908 <uni070@naver.com>
2025-08-20 12:30:21 +0900

    feat: #474 최신 토론 목록 API 관련 응답 모델 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/network/response/latest/AuthorResponse.kt
@@ -0,0 +1,22 @@
+package com.team.todoktodok.data.network.response.latest
+
+
+import com.team.domain.model.latest.Author
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AuthorResponse(
+    val email: String,
+    val id: Int,
+    val nickname: String,
+    val profileImage: String,
+) {
+    fun toDomain(): Author {
+        return Author(
+            email = email,
+            id = id,
+            nickname = nickname,
+            profileImage = profileImage
+        )
+    }
+}
---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/network/response/latest/LatestDiscussionsResponse.kt
@@ -0,0 +1,35 @@
+package com.team.todoktodok.data.network.response.latest
+
+
+import com.team.domain.model.latest.LatestDiscussion
+import com.team.domain.model.latest.LatestDiscussionPage
+import com.team.todoktodok.data.core.ext.toLocalDateTime
+import com.team.todoktodok.data.network.response.discussion.toDomain
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LatestDiscussionsResponse(
+    val latestDiscussionResponses: List<LatestDiscussionResponse>,
+    val pageInfoResponse: PageInfoResponse
+){
+    fun toDomain(): LatestDiscussionPage{
+        val discussion =  latestDiscussionResponses.map {
+            LatestDiscussion(
+                authorResponse = it.author.toDomain(),
+                book = it.book.toDomain(),
+                commentCount = it.commentCount,
+                content = it.content,
+                createdAt = it.createdAt.toLocalDateTime(),
+                discussionId = it.discussionId,
+                isLikedByMe = it.isLikedByMe,
+                likeCount = it.likeCount,
+                title = it.title
+            )
+        }
+
+        return LatestDiscussionPage(
+            discussions = discussion,
+            pageInfo = pageInfoResponse.toDomain()
+        )
+    }
+}
