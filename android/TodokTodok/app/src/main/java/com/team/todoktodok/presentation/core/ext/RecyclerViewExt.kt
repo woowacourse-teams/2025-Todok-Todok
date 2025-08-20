4b92a0049c07a194052689f83f2f0f21d8d728e0
chanho0908 <uni070@naver.com>
2025-08-20 17:52:13 +0900

    feat: #489 RecyclerView 무한 스크롤 확장함수 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/core/ext/RecyclerViewExt.kt
@@ -0,0 +1,34 @@
+package com.team.todoktodok.presentation.core.ext
+
+import android.widget.AbsListView.OnScrollListener.SCROLL_STATE_IDLE
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+fun RecyclerView.addOnScrollEndListener(
+    threshold: Int = 3,
+    callback: () -> Unit,
+) {
+    addOnScrollListener(
+        object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(
+                recyclerView: RecyclerView,
+                newState: Int,
+            ) {
+                if (newState == SCROLL_STATE_IDLE && recyclerView.hasLessItemThan(threshold)) {
+                    callback()
+                }
+            }
+
+            private fun RecyclerView.hasLessItemThan(threshold: Int): Boolean {
+                if (isLayoutRequested) {
+                    return false
+                }
+                (layoutManager as? LinearLayoutManager)?.let {
+                    val lastVisibleItem = it.findLastVisibleItemPosition()
+                    return lastVisibleItem >= it.itemCount - threshold
+                }
+                return false
+            }
+        },
+    )
+}
