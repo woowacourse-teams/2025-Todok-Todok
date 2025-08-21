13865cac9a2474fdf89783894d52fff65eb3870c
chanho0908 <uni070@naver.com>
2025-08-21 20:43:32 +0900

    feat: #513 RecyclerView 확장 함수 추가 및 개선
    - RecyclerView에 스크롤 종료 리스너를 추가하는 `addOnScrollEndListener` 확장 함수 추가
    - 기존 `addOnScrollListener`를 사용하여 구현되었던 로직을 새로운 `scrollEndListener` 프로퍼티를 통해 관리하도록 변경
    - `clearOnScrollEndListener` 함수를 추가하여 등록된 스크롤 종료 리스너를 제거할 수 있도록 함
    - `ids.xml`에 `scroll_listener_tag` ID 추가

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/core/ext/RvExt.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/core/ext/RvExt.kt
@@ -1,34 +1,47 @@
 package com.team.todoktodok.presentation.core.ext
 
-import android.widget.AbsListView.OnScrollListener.SCROLL_STATE_IDLE
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.team.todoktodok.R
+
+private var RecyclerView.scrollEndListener: RecyclerView.OnScrollListener?
+    get() = getTag(R.id.scroll_listener_tag) as? RecyclerView.OnScrollListener
+    set(value) {
+        setTag(R.id.scroll_listener_tag, value)
+    }
 
 fun RecyclerView.addOnScrollEndListener(
     threshold: Int = 3,
     callback: () -> Unit,
 ) {
-    addOnScrollListener(
+    scrollEndListener?.let { removeOnScrollListener(it) }
+
+    val listener =
         object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(
                 recyclerView: RecyclerView,
                 newState: Int,
             ) {
-                if (newState == SCROLL_STATE_IDLE && recyclerView.hasLessItemThan(threshold)) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE && recyclerView.hasLessItemThan(threshold)) {
                     callback()
                 }
             }
 
             private fun RecyclerView.hasLessItemThan(threshold: Int): Boolean {
-                if (isLayoutRequested) {
-                    return false
-                }
+                if (isLayoutRequested) return false
                 (layoutManager as? LinearLayoutManager)?.let {
                     val lastVisibleItem = it.findLastVisibleItemPosition()
                     return lastVisibleItem >= it.itemCount - threshold
                 }
                 return false
             }
-        },
-    )
+        }
+
+    addOnScrollListener(listener)
+    scrollEndListener = listener
+}
+
+fun RecyclerView.clearOnScrollEndListener() {
+    scrollEndListener?.let { removeOnScrollListener(it) }
+    scrollEndListener = null
 }
