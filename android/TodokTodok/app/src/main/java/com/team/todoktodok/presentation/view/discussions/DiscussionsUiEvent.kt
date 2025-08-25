6fcb3d03ae900891b17d32c60a74946db3adb6cf
chanho0908 <uni070@naver.com>
2025-08-25 11:33:16 +0900

    refactor: #538 DiscussionsUiEvent에서 불필요한 이벤트 제거

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/discussions/DiscussionsUiEvent.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/discussions/DiscussionsUiEvent.kt
@@ -3,10 +3,6 @@ package com.team.todoktodok.presentation.view.discussions
 import com.team.domain.model.exception.TodokTodokExceptions
 
 sealed interface DiscussionsUiEvent {
-    data object ShowNotHasAllDiscussions : DiscussionsUiEvent
-
-    data object ShowHasAllDiscussions : DiscussionsUiEvent
-
     data class ShowErrorMessage(
         val exception: TodokTodokExceptions,
     ) : DiscussionsUiEvent
