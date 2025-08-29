56d03c6c8e3a4a945d8c4af03b054be81a2785f1
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-29 15:46:11 +0900

    refactor: #546 SelectBookViewModel에서 중복 로직 삭제로 인한 도서 검색 기능 개선

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/vm/SelectBookViewModel.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/vm/SelectBookViewModel.kt
@@ -4,15 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.team.domain.model.Books
+import com.team.domain.model.book.AladinBook
+import com.team.domain.model.book.AladinBooks
+import com.team.domain.model.exception.BookException
 import com.team.domain.model.exception.TodokTodokExceptions
 import com.team.domain.model.exception.onFailure
 import com.team.domain.model.exception.onSuccess
 import com.team.domain.repository.BookRepository
-import com.team.domain.repository.DiscussionRepository
 import com.team.todoktodok.presentation.core.event.MutableSingleLiveData
 import com.team.todoktodok.presentation.core.event.SingleLiveData
-import com.team.todoktodok.presentation.view.book.SelectBookErrorType
 import com.team.todoktodok.presentation.view.book.SelectBookUiEvent
 import com.team.todoktodok.presentation.view.book.SelectBookUiState
 import kotlinx.coroutines.launch
@@ -27,60 +27,38 @@ class SelectBookViewModel(
     val uiEvent: SingleLiveData<SelectBookUiEvent> get() = _uiEvent
 
     fun searchWithCurrentKeyword(keyword: String) {
-        val isPossibleSearchKeyword = checkKeyword(keyword)
+        val isPossibleSearchKeyword = isPossibleSearchKeyword(keyword)
         updateKeyword(keyword)
-        if (isPossibleSearchKeyword) updateSearchedBooks()
+        if (isPossibleSearchKeyword) updateSearchedBooks(keyword)
     }
 
-    fun updateSelectedBook(position: Int) {
-        if (_uiState.value?.isExist(position) == false) {
-            _uiEvent.setValue(SelectBookUiEvent.ShowError(SelectBookErrorType.ERROR_NO_SELECTED_BOOK))
-            return
-        }
-        val selectedBook =
-            _uiState.value?.searchedBooks?.get(position) ?: run {
-                _uiEvent.setValue(SelectBookUiEvent.ShowError(SelectBookErrorType.ERROR_NO_SELECTED_BOOK))
-                return
-            }
-        _uiState.value = _uiState.value?.copy(selectedBook = selectedBook)
-        _uiEvent.setValue(SelectBookUiEvent.NavigateToCreateDiscussionRoom(selectedBook))
-    }
-
-    fun updateKeyword(keyword: String) {
+    private fun updateKeyword(keyword: String) {
         _uiState.value = _uiState.value?.copy(keyword = keyword)
     }
 
-    private fun checkKeyword(keyword: String): Boolean {
-        if (keyword.isBlank()) {
-            // api 호출이 안됨
-            return false
-        }
-        if (keyword == _uiState.value?.keyword) {
-            // api 호출이 안됨
-            return false
-        }
-        return true
-    }
+    private fun isPossibleSearchKeyword(keyword: String): Boolean =
+        !(keyword.isBlank() || keyword.isEmpty() || _uiState.value?.isSameKeyword(keyword) == true)
 
-    private fun updateSearchedBooks() {
-        val keyword = _uiState.value?.keyword ?: return
+    private fun updateSearchedBooks(keyword: String) {
         _uiState.value = _uiState.value?.copy(isLoading = true)
         viewModelScope.launch {
             bookRepository
                 .fetchBooks(keyword)
-                .onSuccess { books: Books ->
+                .onSuccess { books: AladinBooks ->
                     _uiState.value = _uiState.value?.copy(isLoading = false, searchedBooks = books)
-                    if (books.size == SEARCHED_BOOKS_IS_EMPTY) {
-
-                    }
                 }.onFailure { exception: TodokTodokExceptions ->
                     _uiState.value = _uiState.value?.copy(isLoading = false)
-                    _uiEvent.setValue(SelectBookUiEvent.ShowTodokTodokException(exception))
+                    _uiEvent.setValue(SelectBookUiEvent.ShowException(exception))
                 }
         }
     }
 
-    companion object {
-        private const val SEARCHED_BOOKS_IS_EMPTY: Int = 0
+    fun updateSelectedBook(position: Int) {
+        val selectedBook: AladinBook? = _uiState.value?.selectedBook(position)
+        if (selectedBook == null) {
+            _uiEvent.setValue(SelectBookUiEvent.ShowException(BookException.EmptySelectedBook))
+            return
+        }
+        _uiEvent.setValue(SelectBookUiEvent.NavigateToCreateDiscussionRoom(selectedBook))
     }
 }
