f972f4da012aa8d9dbcde9fec9a800877b126e2e
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-29 15:44:17 +0900

    refactor: #546 SelectBookUiState의 selectedBook 로직 수정

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/SelectBookUiState.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/SelectBookUiState.kt
@@ -1,13 +1,19 @@
 package com.team.todoktodok.presentation.view.book
 
-import com.team.domain.model.Book
-import com.team.domain.model.Books
+import com.team.domain.model.book.AladinBook
+import com.team.domain.model.book.AladinBooks
 
 data class SelectBookUiState(
     val isLoading: Boolean = false,
     val keyword: String = "",
-    val searchedBooks: Books = Books(emptyList()),
-    val selectedBook: Book? = null,
+    val searchedBooks: AladinBooks = AladinBooks(emptyList()),
 ) {
+    fun selectedBook(position: Int): AladinBook? {
+        if (isExist(position)) return searchedBooks[position]
+        return null
+    }
+
     fun isExist(position: Int): Boolean = searchedBooks.isExist(position)
+
+    fun isSameKeyword(keyword: String): Boolean = this.keyword == keyword
 }
