c02a9f6e1e656bfafbf98a74655af5f0444895b0
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-29 15:44:52 +0900

    refactor: #546 사용되지 않는 SelectBookErrorType enum 삭제

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/book/SelectBookErrorType.kt
+++
@@ -1,9 +0,0 @@
-package com.team.todoktodok.presentation.view.book
-
-import com.team.todoktodok.R
-
-enum class SelectBookErrorType(
-    val id: Int,
-) {
-    ERROR_NO_SELECTED_BOOK(R.string.error_no_selected_book),
-}
