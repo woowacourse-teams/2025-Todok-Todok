84dcec0f73525208d06af114ed26505845b175f3
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-30 19:48:47 +0900

    refactor: #546 AladinBook 프로퍼티 네이밍 변경 및 테스트 추가

--- android/TodokTodok/domain/src/main/java/com/team/domain/model/book/AladinBook.kt
+++ android/TodokTodok/domain/src/main/java/com/team/domain/model/book/AladinBook.kt
@@ -1,14 +1,14 @@
 package com.team.domain.model.book
 
 data class AladinBook(
-    private val _id: ISBN,
+    private val _isbn: ISBN,
     private val _title: BookTitle,
     private val _author: BookAuthor,
     private val _image: BookImage,
 ) {
-    val id: String get() = _id.value
+    val isbn: Long get() = _isbn.value
 
-    val title: String get() = _title.mainTitle
+    val mainTitle: String get() = _title.mainTitle
 
     val author: String get() = _author.value
 
@@ -16,13 +16,13 @@ data class AladinBook(
 
     companion object {
         fun AladinBook(
-            id: String,
+            isbn: String,
             title: String,
             author: String,
             image: String,
         ): AladinBook =
             AladinBook(
-                _id = ISBN(id),
+                _isbn = ISBN(isbn.toLong()),
                 _title = BookTitle(title),
                 _author = BookAuthor(author),
                 _image = BookImage(image),
