29d004f15d0710b771ce4dacbcd54f3043292f19
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-30 22:59:48 +0900

    test: #546 SelectBookViewModel 테스트 코드 수정

--- android/TodokTodok/app/src/test/java/com/team/todoktodok/presentation/vm/SelectBookViewModelTest.kt
+++ android/TodokTodok/app/src/test/java/com/team/todoktodok/presentation/vm/SelectBookViewModelTest.kt
@@ -1,22 +1,19 @@
 package com.team.todoktodok.presentation.vm
 
-import com.team.domain.model.Book
-import com.team.domain.model.Books
+import com.team.domain.model.book.AladinBook.Companion.AladinBook
+import com.team.domain.model.book.AladinBooks
 import com.team.domain.model.exception.NetworkResult
 import com.team.domain.repository.BookRepository
-import com.team.domain.repository.DiscussionRepository
 import com.team.todoktodok.CoroutinesTestExtension
 import com.team.todoktodok.InstantTaskExecutorExtension
 import com.team.todoktodok.ext.getOrAwaitValue
-import com.team.todoktodok.presentation.view.book.SelectBookErrorType
+import com.team.todoktodok.presentation.view.book.SearchedBookResultStatus
 import com.team.todoktodok.presentation.view.book.SelectBookUiEvent
 import com.team.todoktodok.presentation.view.book.vm.SelectBookViewModel
 import io.mockk.coEvery
-import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -27,50 +24,80 @@ import org.junit.jupiter.api.extension.ExtendWith
 @ExtendWith(InstantTaskExecutorExtension::class)
 class SelectBookViewModelTest {
     private lateinit var bookRepository: BookRepository
-    private lateinit var discussionRepository: DiscussionRepository
     private lateinit var viewModel: SelectBookViewModel
 
     @BeforeEach
     fun setUp() {
         bookRepository = mockk()
-        discussionRepository = mockk<DiscussionRepository>(relaxed = true)
-        viewModel = SelectBookViewModel(bookRepository, discussionRepository)
+        viewModel = SelectBookViewModel(bookRepository)
     }
 
     @Test
-    fun `빈 키워드 입력시 ERROR_EMPTY_KEYWORD 이벤트가 발생한다`() =
+    fun `키워드 입력시 SelectBookUiState의 keyword가 업데이트 된다`() =
         runTest {
             // given
-            val keyword = ""
-            coVerify { discussionRepository.hasDiscussion() }
+            val keyword = "오브젝트"
 
             // when
-            viewModel.searchWithCurrentKeyword(keyword)
+            viewModel.updateKeyword(keyword)
 
             // then
-            val actual = viewModel.uiEvent.getOrAwaitValue()
+            val actual = viewModel.uiState.getOrAwaitValue()
             assertEquals(
-                SelectBookUiEvent.ShowError(SelectBookErrorType.ERROR_EMPTY_KEYWORD),
-                actual,
+                keyword,
+                actual.keyword,
             )
         }
 
     @Test
-    fun `전과 동일한 키워드 입력시 ERROR_SAME_KEYWORD 이벤트가 발생한다`() =
+    fun `빈 키워드 입력시 도서 리스트를 찾지 않는다`() =
         runTest {
             // given
-            viewModel.updateKeyword("오브젝트")
-            val keyword = "오브젝트"
-            coVerify { discussionRepository.hasDiscussion() }
+            val keyword = ""
 
             // when
             viewModel.searchWithCurrentKeyword(keyword)
 
             // then
-            val actual = viewModel.uiEvent.getOrAwaitValue()
+            val actual = viewModel.uiState.getOrAwaitValue()
             assertEquals(
-                SelectBookUiEvent.ShowError(SelectBookErrorType.ERROR_SAME_KEYWORD),
-                actual,
+                SearchedBookResultStatus.NotStarted,
+                actual.status,
+            )
+        }
+
+    @Test
+    fun `공백 키워드 입력시 도서 리스트를 찾지 않는다`() =
+        runTest {
+            // given
+            val keyword = "    "
+
+            // when
+            viewModel.searchWithCurrentKeyword(keyword)
+
+            // then
+            val actual = viewModel.uiState.getOrAwaitValue()
+            assertEquals(
+                SearchedBookResultStatus.NotStarted,
+                actual.status,
+            )
+        }
+
+    @Test
+    fun `전과 동일한 키워드 입력시 도서 리스트를 찾지 않는다`() =
+        runTest {
+            // given
+            viewModel.updateKeyword("오브젝트")
+            val keyword = "오브젝트"
+
+            // when
+            viewModel.searchWithCurrentKeyword(keyword)
+
+            // then
+            val actual = viewModel.uiState.getOrAwaitValue()
+            assertEquals(
+                SearchedBookResultStatus.NotStarted,
+                actual.status,
             )
         }
 
@@ -80,10 +107,10 @@ class SelectBookViewModelTest {
             // given
             val keyword = "오브젝트"
             val books =
-                Books(
+                AladinBooks(
                     listOf(
-                        Book(
-                            id = 5L,
+                        AladinBook(
+                            isbn = 9791158391409L,
                             title = "오브젝트",
                             author = "조영호",
                             image = "https://image.aladin.co.kr/product/19368/10/cover200/k972635015_1.jpg",
@@ -98,25 +125,6 @@ class SelectBookViewModelTest {
             // then
             val actual = viewModel.uiState.getOrAwaitValue()
             assertEquals(books, actual.searchedBooks)
-            assertThat(actual.isLoading).isFalse()
-        }
-
-    @Test
-    fun `유효하지 않는 포지션의 책을 선택하면, ERROR_NO_SELECTED_BOOK 이벤트가 발생한다`() =
-        runTest {
-            // given
-            val position = -1
-            coVerify { discussionRepository.hasDiscussion() }
-
-            // when
-            viewModel.updateSelectedBook(position)
-
-            // then
-            val event = viewModel.uiEvent.getOrAwaitValue()
-            assertEquals(
-                SelectBookUiEvent.ShowError(SelectBookErrorType.ERROR_NO_SELECTED_BOOK),
-                event,
-            )
         }
 
     @Test
@@ -125,14 +133,14 @@ class SelectBookViewModelTest {
             // given
             val keyword = "오브젝트"
             val book =
-                Book(
-                    id = 5L,
+                AladinBook(
+                    isbn = 9791158391409L,
                     title = "오브젝트",
                     author = "조영호",
                     image = "https://image.aladin.co.kr/product/19368/10/cover200/k972635015_1.jpg",
                 )
             val books =
-                Books(
+                AladinBooks(
                     listOf(
                         book,
                     ),
