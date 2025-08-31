c44cff5bd844d138d2924b5543b35e243fcce0df
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-08-31 10:04:38 +0900

    fix: #546 AladinBook ISBN 필드 타입을 Long으로 변경

--- android/TodokTodok/domain/src/test/java/com/team/domain/model/book/AladinBookTest.kt
+++ android/TodokTodok/domain/src/test/java/com/team/domain/model/book/AladinBookTest.kt
@@ -41,7 +41,7 @@ class AladinBookTest {
         // given
         val book =
             AladinBook.AladinBook(
-                "9791158391409",
+                9791158391409L,
                 "오브젝트 - 코드로 이해하는 객체지향 설계",
                 "조영호 (지은이)",
                 "https://image.aladin.co.kr/product/19368/10/coversum/k972635015_1.jpg",
