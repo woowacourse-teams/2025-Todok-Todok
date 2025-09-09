bebe7ad92c0829b64a37aad80ee0c91b678a2eaf
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-09 15:15:16 +0900

    feat: #604 알림 앱바 구현

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/theme/Color.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val Black18 = Color(0xFF181A1A)
+
+val Green1A = Color(0xFF1AB25C)
+
+val RedFF = Color(0xFFFF0000)
+
+val Gray66 = Color(0xFF666666)
+
+val Gray19 = Color(0x19999999)
