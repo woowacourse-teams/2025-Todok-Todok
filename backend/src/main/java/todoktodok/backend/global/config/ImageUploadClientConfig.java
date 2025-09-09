cef2f0c11d4e4156b7f890fbf2d4a16d7c9ce5da
ljhee92 <weilainib7@gmail.com>
2025-09-09 21:37:43 +0900

    refactor: region 프로퍼티로 수정 및 업로드 한도 설정

--- backend/src/main/java/todoktodok/backend/global/config/ImageUploadClientConfig.java
+++ backend/src/main/java/todoktodok/backend/global/config/ImageUploadClientConfig.java
@@ -14,9 +14,11 @@ public class ImageUploadClientConfig {
     @Bean
     public S3ImageUploadClient s3ImageUploadClient(
             @Value("${cloud.aws.s3.bucket}") final String bucketName,
-            @Value("${cloud.aws.s3.key-prefix}") final String keyPrefix
+            @Value("${cloud.aws.s3.key-prefix}") final String keyPrefix,
+            @Value("${cloud.aws.s3.region}") final String regionValue
+
     ) {
-        final Region region = Region.AP_NORTHEAST_2;
+        final Region region = Region.of(regionValue);
         final S3Client s3Client = S3Client.builder()
                 .region(region)
                 .credentialsProvider(DefaultCredentialsProvider.create())
