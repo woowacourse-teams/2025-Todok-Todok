c6deed0964779166652d47e7d177e6198c00e7ff
Chaeyoung714 <202102760@hufs.ac.kr>
2025-09-12 19:32:56 +0900

    feat: findAnyById 메서드 추가

--- backend/src/main/java/todoktodok/backend/member/domain/repository/MemberRepository.java
+++ backend/src/main/java/todoktodok/backend/member/domain/repository/MemberRepository.java
@@ -2,9 +2,18 @@ package todoktodok.backend.member.domain.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import todoktodok.backend.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query(value = """
+        SELECT *
+        FROM member
+        WHERE id = :id
+        """, nativeQuery = true)
+    Optional<Member> findAnyById(@Param("id") final Long id);
 
     Optional<Member> findByEmail(final String email);
 
