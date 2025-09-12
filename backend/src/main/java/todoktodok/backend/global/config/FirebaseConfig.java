00373f7118220a0ee16e9d77ddfce7ff0d37b9d0
Chaeyoung714 <202102760@hufs.ac.kr>
2025-09-12 20:47:07 +0900

    chore: FirebaseConfig에서 파일 읽기 경로 및 로직 수정

--- backend/src/main/java/todoktodok/backend/global/config/FirebaseConfig.java
+++ backend/src/main/java/todoktodok/backend/global/config/FirebaseConfig.java
@@ -8,21 +8,20 @@ import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class FirebaseConfig {
 
     @Value("${firebase.account.path}")
-    private String filePath;
+    private Resource serviceAccountJson;
 
     @Bean
     public FirebaseApp initialize() throws IOException {
-        try (final InputStream serviceAccount = new ClassPathResource(filePath).getInputStream()) {
-            final FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        try (InputStream in = serviceAccountJson.getInputStream()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(in))
                     .build();
-
             return FirebaseApp.initializeApp(options);
         }
     }
