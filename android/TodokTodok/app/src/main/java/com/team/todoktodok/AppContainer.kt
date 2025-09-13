fdd76343aa338a09955dab4ba990dcb488cdc467
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-13 19:21:06 +0900

    feat : #638 AppContainer에 LocalDatabaseModule 추가

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/AppContainer.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/AppContainer.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.lifecycleScope
 import com.team.todoktodok.data.datasource.token.TokenDataSource
 import com.team.todoktodok.data.datasource.token.TokenLocalDataSource
 import com.team.todoktodok.data.di.DataSourceModule
+import com.team.todoktodok.data.di.LocalDatabaseModule
 import com.team.todoktodok.data.di.OkhttpModule
 import com.team.todoktodok.data.di.RepositoryModule
 import com.team.todoktodok.data.di.RetrofitModule
@@ -14,7 +15,6 @@ import com.team.todoktodok.data.network.auth.AuthInterceptor
 import com.team.todoktodok.data.network.auth.TokenAuthenticator
 import com.team.todoktodok.data.network.auth.TokenRefreshDelegate
 import com.team.todoktodok.data.network.auth.TokenRefreshDelegator
-import kotlin.getValue
 
 class AppContainer(
     context: Context,
@@ -48,9 +48,11 @@ class AppContainer(
     private val serviceModule: ServiceModule by lazy {
         ServiceModule(retrofitModule)
     }
-
+    private val localDatabaseModule: LocalDatabaseModule by lazy {
+        LocalDatabaseModule(context)
+    }
     private val dataSourceModule: DataSourceModule by lazy {
-        DataSourceModule(serviceModule, context)
+        DataSourceModule(serviceModule, localDatabaseModule, context)
     }
 
     private val okHttpModule: OkhttpModule by lazy {
