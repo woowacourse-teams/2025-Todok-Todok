44f63c9c23a0eb3ac953ab9037d904f89ff737b4
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-13 19:20:04 +0900

    feat: #638 회원가입 시 푸시 알림 등록 기능 추가

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/auth/signup/vm/SignUpViewModelFactory.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/auth/signup/vm/SignUpViewModelFactory.kt
@@ -3,13 +3,15 @@ package com.team.todoktodok.presentation.view.auth.signup.vm
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.team.domain.repository.MemberRepository
+import com.team.domain.repository.NotificationRepository
 
 class SignUpViewModelFactory(
     private val memberRepository: MemberRepository,
+    private val notificationRepository: NotificationRepository,
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(SignUpViewModel::class.java)) {
-            return SignUpViewModel(memberRepository) as T
+            return SignUpViewModel(memberRepository, notificationRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
