e313bf552d6a6f8d4b32edfb1ab9680a64d9874a
chanho0908 <uni070@naver.com>
2025-09-13 12:18:34 +0900

    feat: #610 설정 화면 로그아웃 기능 구현

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/setting/SettingFragment.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/setting/SettingFragment.kt
@@ -6,14 +6,24 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.team.todoktodok.App
 import com.team.todoktodok.BuildConfig
 import com.team.todoktodok.R
 import com.team.todoktodok.databinding.FragmentSettingBinding
+import com.team.todoktodok.presentation.core.component.AlertSnackBar.Companion.AlertSnackBar
+import com.team.todoktodok.presentation.core.component.CommonDialog
+import com.team.todoktodok.presentation.view.auth.AuthActivity
+import com.team.todoktodok.presentation.view.auth.login.GoogleCredentialManager
 import com.team.todoktodok.presentation.view.setting.vm.SettingViewModel
 import com.team.todoktodok.presentation.view.setting.vm.SettingViewModelFactory
+import kotlinx.coroutines.launch
 
 class SettingFragment : Fragment(R.layout.fragment_setting) {
-    private val viewModel: SettingViewModel by activityViewModels { SettingViewModelFactory() }
+    private val viewModel: SettingViewModel by activityViewModels {
+        val repositoryModule = (requireActivity().application as App).container.repositoryModule
+        SettingViewModelFactory(repositoryModule.tokenRepository)
+    }
 
     override fun onViewCreated(
         view: View,
@@ -22,6 +32,8 @@ class SettingFragment : Fragment(R.layout.fragment_setting) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentSettingBinding.bind(view)
         initView(binding)
+        setUpFragmentResultToLogout(binding)
+        setupUiEvent()
     }
 
     private fun initView(binding: FragmentSettingBinding) {
@@ -42,6 +54,60 @@ class SettingFragment : Fragment(R.layout.fragment_setting) {
             llWithdraw.setOnClickListener {
                 viewModel.changeScreen(SettingScreen.WITHDRAW)
             }
+
+            llLogout.setOnClickListener {
+                CommonDialog
+                    .newInstance(
+                        message = getString(R.string.logout_ask),
+                        submitButtonText =
+                            getString(
+                                (R.string.text_logout),
+                            ),
+                    ).show(parentFragmentManager, CommonDialog.TAG)
+            }
         }
+    }
+
+    private fun setUpFragmentResultToLogout(binding: FragmentSettingBinding) {
+        parentFragmentManager.setFragmentResultListener(
+            CommonDialog.REQUEST_KEY_COMMON_DIALOG,
+            viewLifecycleOwner,
+        ) { _, bundle ->
+            val isConfirmed = bundle.getBoolean(CommonDialog.RESULT_KEY_COMMON_DIALOG, false)
+            if (isConfirmed) logout(binding)
+        }
+    }
+
+    private fun logout(binding: FragmentSettingBinding) {
+        val credentialManager =
+            GoogleCredentialManager(requireActivity(), BuildConfig.GOOGLE_CLIENT_ID)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            credentialManager
+                .logOut()
+                .onSuccess { viewModel.logout() }
+                .onFailure {
+                    AlertSnackBar(
+                        view = binding.root,
+                        messageRes = R.string.logout_fail,
+                    ).show()
+                }
+        }
+    }
+
+    private fun setupUiEvent() {
+        viewModel.uiEvent.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                SettingUiEvent.NavigateToLogin -> moveToLogin()
+            }
+        }
+    }
+
+    private fun moveToLogin() {
+        val intent =
+            AuthActivity.Intent(requireActivity()).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+        startActivity(intent)
     }
 }
