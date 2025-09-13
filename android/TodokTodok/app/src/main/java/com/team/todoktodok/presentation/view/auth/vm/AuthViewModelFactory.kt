d59988cfb20981154cd625653e2146d31770da61
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-13 19:20:28 +0900

    feat: #638 로그인 성공 시 푸시 알림 등록 기능 추가

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/auth/vm/AuthViewModelFactory.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/auth/vm/AuthViewModelFactory.kt
@@ -3,15 +3,17 @@ package com.team.todoktodok.presentation.view.auth.vm
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.team.domain.repository.MemberRepository
+import com.team.domain.repository.NotificationRepository
 import com.team.domain.repository.TokenRepository
 
 class AuthViewModelFactory(
     private val memberRepository: MemberRepository,
     private val tokenRepository: TokenRepository,
+    private val notificationRepository: NotificationRepository,
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(AuthViewModel::class.java)) {
-            return AuthViewModel(memberRepository, tokenRepository) as T
+            return AuthViewModel(memberRepository, tokenRepository, notificationRepository) as T
         } else {
             throw IllegalArgumentException()
         }
