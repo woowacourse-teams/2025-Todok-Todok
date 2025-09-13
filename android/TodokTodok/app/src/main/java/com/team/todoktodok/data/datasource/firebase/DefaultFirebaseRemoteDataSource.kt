5811e3a23e4ade710b42b4f68c18efa22a144663
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-13 21:11:49 +0900

    fix: #638 fcmToken 불러오는 로직 수정

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/data/datasource/firebase/DefaultFirebaseRemoteDataSource.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/datasource/firebase/DefaultFirebaseRemoteDataSource.kt
@@ -5,7 +5,7 @@ import com.team.todoktodok.data.network.service.FirebaseService
 class DefaultFirebaseRemoteDataSource(
     private val firebaseService: FirebaseService,
 ) : FirebaseRemoteDataSource {
-    override fun getFcmToken(): String = firebaseService.getFcmToken()
+    override suspend fun getFcmToken(): String = firebaseService.getFcmToken()
 
     override fun getFId(): String = firebaseService.getFId()
 }
