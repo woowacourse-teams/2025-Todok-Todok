f755724d55753055756ff7cd8a242086d5ba4fb2
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-13 19:16:26 +0900

    feat: #638 FCM 토큰 및 ID 관리를 위한 데이터 소스 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/datasource/firebase/DefaultFirebaseRemoteDataSource.kt
@@ -0,0 +1,11 @@
+package com.team.todoktodok.data.datasource.firebase
+
+class DefaultFirebaseRemoteDataSource : FirebaseRemoteDataSource {
+    override fun getFcmToken(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getFId(): String {
+        TODO("Not yet implemented")
+    }
+}
