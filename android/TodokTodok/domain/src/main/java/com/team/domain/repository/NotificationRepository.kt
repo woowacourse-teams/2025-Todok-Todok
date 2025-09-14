1c8de69e559a89b7ca5717d571c19baf302ca5a1
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-14 11:28:52 +0900

    feat: #638 알림 저장 및 조회 기능 구현

--- android/TodokTodok/domain/src/main/java/com/team/domain/repository/NotificationRepository.kt
+++ android/TodokTodok/domain/src/main/java/com/team/domain/repository/NotificationRepository.kt
@@ -1,9 +1,19 @@
 package com.team.domain.repository
 
 import com.team.domain.model.exception.NetworkResult
+import com.team.domain.model.notification.Notification
 
 interface NotificationRepository {
-    suspend fun registerPushNotification(): NetworkResult<Unit>
+    suspend fun registerPushNotification(
+        token: String,
+        fId: String,
+    ): NetworkResult<Unit>
 
     suspend fun deletePushNotification()
+
+    suspend fun getNotifications(): List<Notification>
+
+    suspend fun saveNotification(notification: Notification): Long
+
+    suspend fun getNotificationCount(): Int
 }
