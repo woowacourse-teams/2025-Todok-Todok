f82ff662c3ca07d8f60f703d93883e14d6e9319e
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-14 15:56:29 +0900

    feat: #638 알림 목록 조회 기능 구현

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/notification/vm/NotificationViewModel.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/view/notification/vm/NotificationViewModel.kt
@@ -1,8 +1,30 @@
 package com.team.todoktodok.presentation.view.notification.vm
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.team.domain.repository.NotificationRepository
+import com.team.todoktodok.presentation.view.notification.NotificationUiState
+import kotlinx.coroutines.launch
 
 class NotificationViewModel(
     private val notificationRepository: NotificationRepository,
-) : ViewModel()
+) : ViewModel() {
+    private val _uiState: MutableLiveData<NotificationUiState> =
+        MutableLiveData(NotificationUiState())
+    val uiState: LiveData<NotificationUiState> get() = _uiState
+
+    init {
+        initNotifications()
+    }
+
+    private fun initNotifications() {
+        viewModelScope.launch {
+            _uiState.value = _uiState.value?.copy(isLoading = true)
+            val notifications = notificationRepository.getNotifications()
+            _uiState.value =
+                _uiState.value?.copy(isLoading = false, notifications = notifications)
+        }
+    }
+}
