d1de588afbe3aee6eda4036ffaea3f37c49f6c11
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-14 11:28:52 +0900

    feat: #638 알림 저장 및 조회 기능 구현

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/data/repository/DefaultNotificationRepository.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/repository/DefaultNotificationRepository.kt
@@ -1,39 +1,44 @@
 package com.team.todoktodok.data.repository
 
-import android.util.Log
 import com.team.domain.model.exception.NetworkResult
+import com.team.domain.model.notification.Notification
 import com.team.domain.repository.NotificationRepository
-import com.team.todoktodok.data.datasource.firebase.FirebaseRemoteDataSource
 import com.team.todoktodok.data.datasource.notification.NotificationLocalDataSource
 import com.team.todoktodok.data.datasource.notification.NotificationRemoteDataSource
+import com.team.todoktodok.data.local.nofitication.toDomain
+import com.team.todoktodok.data.local.nofitication.toEntity
 
 class DefaultNotificationRepository(
     private val notificationRemoteDataSource: NotificationRemoteDataSource,
     private val notificationLocalDataSource: NotificationLocalDataSource,
-    private val firebaseRemoteDataSource: FirebaseRemoteDataSource,
 ) : NotificationRepository {
-    override suspend fun registerPushNotification(): NetworkResult<Unit> {
-        notificationLocalDataSource.getFcmToken()
-        notificationLocalDataSource.getFId()
+    override suspend fun registerPushNotification(
+        token: String,
+        fId: String,
+    ): NetworkResult<Unit> {
+        val storedFcmToken = notificationLocalDataSource.getFcmToken()
+        val storedFcmFId = notificationLocalDataSource.getFId()
 
-        val freshFcmToken = firebaseRemoteDataSource.getFcmToken()
-        val freshFcmFId = firebaseRemoteDataSource.getFId()
+        val isNeedRegister =
+            isNeedRegister(storedFcmToken, storedFcmFId, token, fId)
 
-        Log.d("test", "freshFcmToken: $freshFcmToken")
-        Log.d("test", "freshFcmToken: $freshFcmFId")
-//        val isNeedRegister =
-//            isNeedRegister(storedFcmToken, storedFcmFId, freshFcmToken, freshFcmFId)
-//
-//        if (isNeedRegister) {
-//            saveNewPushNotificationToLocal(freshFcmToken, freshFcmFId)
-//            return notificationRemoteDataSource.saveFcmToken(freshFcmToken, freshFcmFId)
-//        }
+        if (isNeedRegister) {
+            saveNewPushNotificationToLocal(token, fId)
+            return notificationRemoteDataSource.saveFcmToken(token, fId)
+        }
         return NetworkResult.Success(Unit)
     }
 
     override suspend fun deletePushNotification() {
         notificationLocalDataSource.deletePushNotification()
     }
+
+    override suspend fun getNotifications(): List<Notification> = notificationLocalDataSource.getNotifications().map { it.toDomain() }
+
+    override suspend fun saveNotification(notification: Notification): Long =
+        notificationLocalDataSource.saveNotifications(notification.toEntity())
+
+    override suspend fun getNotificationCount(): Int = notificationLocalDataSource.getNotificationCount()
 
     private suspend fun saveNewPushNotificationToLocal(
         fcmToken: String,
