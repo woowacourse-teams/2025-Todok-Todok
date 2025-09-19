85ac7aa2a85b89feab29b0c99efecd4dfc56542c
chanho0908 <uni070@naver.com>
2025-09-19 19:02:24 +0900

    feat: 네트워크 상태 관찰 기능 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/connectivity/NetworkConnectivityObserver.kt
@@ -0,0 +1,63 @@
+package com.team.todoktodok.connectivity
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import com.team.domain.ConnectivityObserver
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+@OptIn(DelicateCoroutinesApi::class)
+class NetworkConnectivityObserver(
+    context: Context,
+) : ConnectivityObserver {
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    override fun subscribe(): StateFlow<ConnectivityObserver.Status> =
+        observe().stateIn(
+            GlobalScope,
+            WhileSubscribed(5000),
+            ConnectivityObserver.Status.Idle,
+        )
+
+    override fun value(): ConnectivityObserver.Status {
+        val network = connectivityManager.activeNetwork
+        val capabilities = connectivityManager.getNetworkCapabilities(network)
+
+        val status =
+            capabilities?.let { ConnectivityObserver.Status.Available }
+                ?: ConnectivityObserver.Status.Lost
+        return status
+    }
+
+    private fun observe(): Flow<ConnectivityObserver.Status> =
+        callbackFlow {
+            val callback =
+                object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        super.onAvailable(network)
+                        launch { send(ConnectivityObserver.Status.Available) }
+                    }
+
+                    override fun onLost(network: Network) {
+                        super.onLost(network)
+                        launch { send(ConnectivityObserver.Status.Lost) }
+                    }
+                }
+
+            connectivityManager.registerDefaultNetworkCallback(callback)
+
+            awaitClose {
+                connectivityManager.unregisterNetworkCallback(callback)
+            }
+        }.distinctUntilChanged()
+}
