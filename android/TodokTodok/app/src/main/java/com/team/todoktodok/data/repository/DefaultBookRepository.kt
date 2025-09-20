c5049ce6e866f48e87d0e0c998200f91d7f59b18
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-20 14:13:20 +0900

    refactor: #653 페이지네이션을 통해서, 서버에서 도서 검색 결과를 불러오도록 Repository 레이어 구현

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/data/repository/DefaultBookRepository.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/repository/DefaultBookRepository.kt
@@ -2,24 +2,42 @@ package com.team.todoktodok.data.repository
 
 import com.team.domain.model.Book
 import com.team.domain.model.book.Keyword
-import com.team.domain.model.book.SearchedBooks
+import com.team.domain.model.book.SearchedBooksResult
 import com.team.domain.model.exception.NetworkResult
 import com.team.domain.model.exception.map
 import com.team.domain.repository.BookRepository
 import com.team.todoktodok.data.datasource.book.BookRemoteDataSource
 import com.team.todoktodok.data.network.request.toRequest
-import com.team.todoktodok.data.network.response.book.SearchedBookResponse
 import com.team.todoktodok.data.network.response.book.toDomain
 
 class DefaultBookRepository(
     private val bookRemoteDataSource: BookRemoteDataSource,
 ) : BookRepository {
-    override suspend fun fetchBooks(keyword: Keyword): NetworkResult<SearchedBooks> =
-        bookRemoteDataSource
-            .fetchBooks(keyword.value)
-            .map { SearchedBookResponse: List<SearchedBookResponse> ->
-                SearchedBooks(SearchedBookResponse.map { it.toDomain() })
-            }
+    private var cursor: String? = null
+    private var keyword: Keyword = Keyword("")
+
+    override suspend fun fetchBooks(
+        size: Int,
+        keyword: Keyword,
+    ): NetworkResult<SearchedBooksResult> {
+        if (this.keyword != keyword) {
+            cursor = null
+        }
+
+        this.keyword = keyword
+
+        val result =
+            bookRemoteDataSource
+                .fetchBooks(size, cursor, keyword.value)
+
+        result.map { response ->
+            this.cursor = response.pageInfo.nextCursor
+        }
+
+        return result.map { response ->
+            response.toDomain()
+        }
+    }
 
     override suspend fun saveBook(book: Book): NetworkResult<Long> = bookRemoteDataSource.saveBook(book.toRequest())
 }
--- android/TodokTodok/domain/src/main/java/com/team/domain/repository/BookRepository.kt
+++ android/TodokTodok/domain/src/main/java/com/team/domain/repository/BookRepository.kt
@@ -2,11 +2,14 @@ package com.team.domain.repository
 
 import com.team.domain.model.Book
 import com.team.domain.model.book.Keyword
-import com.team.domain.model.book.SearchedBooks
+import com.team.domain.model.book.SearchedBooksResult
 import com.team.domain.model.exception.NetworkResult
 
 interface BookRepository {
-    suspend fun fetchBooks(keyword: Keyword): NetworkResult<SearchedBooks>
+    suspend fun fetchBooks(
+        size: Int,
+        keyword: Keyword,
+    ): NetworkResult<SearchedBooksResult>
 
     suspend fun saveBook(book: Book): NetworkResult<Long>
 }
