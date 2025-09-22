f14c76d7c25439c97e5143fbce634a76eacc18f6
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-22 12:53:49 +0900

    refactor: #653 `SearchedBooksTotalSizeTest` 테스트 메시지 변경

--- android/TodokTodok/domain/src/main/java/com/team/domain/model/exception/SearchedBooksTotalSizeException.kt
+++ android/TodokTodok/domain/src/main/java/com/team/domain/model/exception/SearchedBooksTotalSizeException.kt
@@ -4,7 +4,7 @@ sealed class SearchedBooksTotalSizeException(
     override val message: String,
 ) : TodokTodokExceptions() {
     data object InvalidSize :
-        SearchedBooksTotalSizeException("[ERROR] 책 검색 요청시 총 책의 개수는 1에서 200까지만 가능합니다.") {
+        SearchedBooksTotalSizeException("[ERROR] 책 검색 요청시 총 책의 개수는 0에서 200까지만 가능합니다.") {
         private fun readResolve(): Any = InvalidSize
     }
 }
--- android/TodokTodok/domain/src/test/java/com/team/domain/model/book/SearchedBooksTotalSizeTest.kt
+++ android/TodokTodok/domain/src/test/java/com/team/domain/model/book/SearchedBooksTotalSizeTest.kt
@@ -18,6 +18,6 @@ class SearchedBooksTotalSizeTest {
     fun `검색된 총 책의 개수가 0~200가 아니면 에러가 난다`(size: Int) {
         val exception =
             assertThrows<IllegalArgumentException> { SearchedBooksTotalSize(size) }
-        assertEquals("[ERROR] 책 검색 요청시 총 책의 개수는 1에서 200까지만 가능합니다.", exception.message)
+        assertEquals("[ERROR] 책 검색 요청시 총 책의 개수는 0에서 200까지만 가능합니다.", exception.message)
     }
 }
