c22cdfb47620d3e5e5790865bdd10aa82663d0dd
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-22 12:48:34 +0900

    refactor: #653 `SelectBookUiState`에서 `isExist` 로직 변경

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/book/SelectBookUiState.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/book/SelectBookUiState.kt
@@ -19,11 +19,11 @@ data class SelectBookUiState(
             listOf(
                 SearchBooksGroup.Count(size),
                 *
-                    searchedBooksResult
-                        ?.books
-                        ?.map { book -> SearchBooksGroup.Book(book) }
-                        ?.toTypedArray()
-                        ?: emptyArray(),
+                searchedBooksResult
+                    ?.books
+                    ?.map { book -> SearchBooksGroup.Book(book) }
+                    ?.toTypedArray()
+                    ?: emptyArray(),
             )
 
     val size: Int get() = searchedBooksResult?.totalSize ?: 0
@@ -35,5 +35,6 @@ data class SelectBookUiState(
 
     fun isSameKeyword(value: String): Boolean = this.keyword == Keyword(value)
 
-    private fun isExist(position: Int): Boolean = searchedBooksResult?.books?.contains(position) ?: false
+    private fun isExist(position: Int): Boolean =
+        searchedBooksResult?.books?.value?.getOrNull(position) != null
 }
