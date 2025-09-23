c0eb8c7385f49cded104f7c8ed7e33fdf1142532
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-23 09:37:13 +0900

    refactor: #653 도서 검색 시 개수 나오는 글씨 폰트 변경

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/book/adapter/SearchBooksCountViewHolder.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/book/adapter/SearchBooksCountViewHolder.kt
@@ -1,7 +1,12 @@
 package com.team.todoktodok.presentation.xml.book.adapter
 
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.team.todoktodok.R
 import com.team.todoktodok.databinding.ItemSearchedBooksCountBinding
@@ -10,8 +15,29 @@ class SearchBooksCountViewHolder private constructor(
     val binding: ItemSearchedBooksCountBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(size: Int) {
-        binding.tvSearchedBooksCount.text =
-            binding.root.context.getString(R.string.select_book_searched_books_count, size)
+        val context = binding.root.context
+        val text = context.getString(R.string.select_book_searched_books_count, size)
+
+        val spannable = SpannableString(text)
+
+        val start = text.indexOf(size.toString())
+        val end = start + size.toString().length
+
+        spannable.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(context, R.color.green_1A)),
+            start,
+            end,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannable.setSpan(
+            StyleSpan(android.graphics.Typeface.BOLD),
+            start,
+            end,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        binding.tvSearchedBooksCount.text = spannable
     }
 
     companion object {
