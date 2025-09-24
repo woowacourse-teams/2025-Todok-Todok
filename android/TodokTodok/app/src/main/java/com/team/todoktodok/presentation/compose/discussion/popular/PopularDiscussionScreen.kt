cf468e6efdc43c81f317233dec226cafba585d75
chanho0908 <uni070@naver.com>
2025-09-24 15:10:07 +0900

    feat: #671 인기 토론 목록 스크린 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/discussion/popular/PopularDiscussionScreen.kt
@@ -0,0 +1,84 @@
+package com.team.todoktodok.presentation.compose.discussion.popular
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.team.todoktodok.R
+import com.team.todoktodok.presentation.compose.component.DiscussionCard
+import com.team.todoktodok.presentation.compose.component.DiscussionCardType
+import com.team.todoktodok.presentation.compose.preview.PopularDiscussionsPreviewParameterProvider
+import com.team.todoktodok.presentation.compose.theme.Black18
+import com.team.todoktodok.presentation.compose.theme.Pretendard
+
+@Composable
+fun PopularDiscussionScreen(
+    onClick: (Long) -> Unit,
+    popularDiscussions: PopularDiscussionsUiState,
+) {
+    Column {
+        PopularDiscussionHeader()
+
+        LazyRow(
+            contentPadding = PaddingValues(5.dp),
+        ) {
+            items(items = popularDiscussions.discussions, key = { it.discussionId }) { item ->
+                DiscussionCard(
+                    uiState = item,
+                    onClick = { onClick(item.discussionId) },
+                    discussionCardType = DiscussionCardType.OpinionVisible,
+                    modifier =
+                        Modifier
+                            .fillParentMaxWidth(0.9f),
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun PopularDiscussionHeader() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            painter = painterResource(R.drawable.img_fire),
+            contentDescription = null,
+            modifier = Modifier.size(16.dp),
+        )
+
+        Text(
+            text = stringResource(R.string.discussions_hot_discussion_rooms),
+            fontSize = 18.sp,
+            fontFamily = Pretendard,
+            fontWeight = SemiBold,
+            color = Black18,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PopularDiscussionScreenPreview(
+    @PreviewParameter(PopularDiscussionsPreviewParameterProvider::class)
+    popularDiscussions: PopularDiscussionsUiState,
+) {
+    PopularDiscussionScreen(
+        onClick = {},
+        popularDiscussions = popularDiscussions,
+    )
+}
