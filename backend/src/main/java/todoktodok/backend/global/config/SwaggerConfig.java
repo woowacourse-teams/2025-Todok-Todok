d98b8cea17b51de96902da2a4dae974f4947bf94
Chaeyoung714 <202102760@hufs.ac.kr>
2025-09-24 15:20:25 +0900

    docs: 스웨거 로그인 안내문구 수정

--- backend/src/main/java/todoktodok/backend/global/config/SwaggerConfig.java
+++ backend/src/main/java/todoktodok/backend/global/config/SwaggerConfig.java
@@ -59,22 +59,17 @@ public class SwaggerConfig {
                 ### API 인증 및 테스트 방법
                 
                 API 테스트 전, 회원가입/로그인 인증 방법은 아래와 같습니다.\n
-                (이미 회원가입이 되어 있는 email이라면 **3~5번 과정**을 생략하시면 됩니다.)
                 
                 ---
-                1. 우측 상단 **Select a definition** -> `unauthenticated` 선택
-                2. **로그인 API**에서 email 입력 -> Execute\n
+                1. 토독토독에서는 안드로이드가 구글에서 발급받은 토큰으로 로그인하고 있기 때문에, 서버에서 자체적으로 로그인을 할 수 없습니다. \n 따라서 **관리자에게 문의** 후 **테스트용 로그인 이메일을 받아**, 관리자용 로그인 방식을 사용해주세요.
+                2. 우측 상단 **Select a definition** -> `unauthenticated` 선택
+                3. **[관리자 전용] 로그인 API**에서 email 입력 -> Execute\n
                    -> 하단 `Response headers`에서 `authorization: Bearer` 뒤 토큰 **모두 복사** (공백 없이!)\n
                    (복사할 토큰 예시: eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwicm9sZSI6IlVTRVIiLCJleHAiOjE3
                    NTU2MTgxNjZ9._-0qTNmPyO1m6LnpEAwkGAB92Es0yBwxNBtmsq_VrGk)
-                3. 우측 상단 **Select a definition** -> `authorization` 선택
-                4. 2번에서 복사한 토큰 -> 우측 초록색 자물쇠 **Authorize** 버튼 클릭 -> Value에 붙여넣기 후 Authorize 버튼 클릭
-                5. **회원가입 API**에서 2번에 입력한 email, 사용할 nickname 입력 -> Execute\n
-                   -> 하단 `Response headers`에서 `authorization: Bearer` 뒤 토큰 **모두 복사** (공백 없이!)\n
-                   (복사할 토큰 예시: eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwicm9sZSI6IlVTRVIiLCJleHAiOjE3
-                   NTU2MTgxNjZ9._-0qTNmPyO1m6LnpEAwkGAB92Es0yBwxNBtmsq_VrGk)
-                6. 2번 또는 5번에서 복사한 토큰 -> 우측 초록색 자물쇠 **Authorize** 버튼 클릭 -> Value에 붙여넣기 후 Authorize 버튼 클릭
-                7. API 테스트 가능!
+                4. 우측 상단 **Select a definition** -> `authorization` 선택
+                5. 3번에서 복사한 토큰 -> 우측 초록색 자물쇠 **Authorize** 버튼 클릭 -> Value에 붙여넣기 후 Authorize 버튼 클릭
+                6. API 테스트 가능!
                 """;
 
         final Info info = new Info().title(title).description(description).version("1.0.0");
