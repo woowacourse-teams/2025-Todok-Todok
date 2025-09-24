e34ef9cfa6a05b52bca1a4e104cbb5baf3a98a1e
Chaeyoung714 <202102760@hufs.ac.kr>
2025-09-24 13:11:31 +0900

    feat: 관리자 이메일 정보 주입

---
+++ backend/src/main/java/todoktodok/backend/global/auth/AdminProperties.java
@@ -0,0 +1,37 @@
+package todoktodok.backend.global.auth;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import todoktodok.backend.member.domain.Member;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class AdminProperties {
+
+    private final Set<String> emails;
+
+    public AdminProperties(
+            @Value("${admin.email-path}") Resource adminEmailResources
+    ) throws IOException {
+        try (InputStream in = adminEmailResources.getInputStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8))) {
+            this.emails = br.lines()
+                    .map(String::trim)
+                    .filter(s -> !s.isEmpty() && !s.startsWith("#"))
+                    .map(String::toLowerCase)
+                    .collect(Collectors.toSet());
+        }
+    }
+
+    public boolean checkIsAdmin(final Member member) {
+        return emails.contains(member.getEmail());
+    }
+}
