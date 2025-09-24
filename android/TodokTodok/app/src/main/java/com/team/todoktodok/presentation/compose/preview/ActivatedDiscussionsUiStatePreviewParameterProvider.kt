4dbef70d04dee854eb597e077c94eac27bd299fc
chanho0908 <uni070@naver.com>
2025-09-24 20:50:15 +0900

    refactor: #671 DiscussionUiStatePreviewParameterProvider`를 사용하여 토론 목록 미리보기 데이터 생성 로직 통합

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/preview/ActivatedDiscussionsUiStatePreviewParameterProvider.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/preview/ActivatedDiscussionsUiStatePreviewParameterProvider.kt
@@ -1,47 +1,14 @@
 package com.team.todoktodok.presentation.compose.preview
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import com.team.domain.model.Book
-import com.team.domain.model.Discussion
-import com.team.domain.model.member.Nickname
-import com.team.domain.model.member.User
 import com.team.todoktodok.presentation.compose.component.DiscussionCardType
 import com.team.todoktodok.presentation.compose.discussion.activate.ActivatedDiscussionsUiState
-import com.team.todoktodok.presentation.xml.discussions.DiscussionUiState
-import java.time.LocalDateTime
 
 class ActivatedDiscussionsUiStatePreviewParameterProvider : PreviewParameterProvider<ActivatedDiscussionsUiState> {
     override val values =
         sequenceOf(
             ActivatedDiscussionsUiState(
-                discussions =
-                    (1L..5L).map { id ->
-                        DiscussionUiState(
-                            item =
-                                Discussion(
-                                    id = id,
-                                    discussionTitle = "토론 주제 $id",
-                                    book =
-                                        Book(
-                                            id = id,
-                                            title = "책 제목 $id",
-                                            author = "저자 $id",
-                                            image = "",
-                                        ),
-                                    writer =
-                                        User(
-                                            id = id,
-                                            nickname = Nickname("사용자$id"),
-                                            profileImage = "",
-                                        ),
-                                    createAt = LocalDateTime.of(2025, 7, 12, 12, 0).plusDays(id),
-                                    discussionOpinion = "이것은 토론 의견 $id 입니다.",
-                                    likeCount = (id * 2).toInt(),
-                                    commentCount = (id * 3).toInt(),
-                                    isLikedByMe = id % 2 == 0L,
-                                ),
-                        )
-                    },
+                discussions = DiscussionUiStatePreviewParameterProvider().values.first(),
                 type = DiscussionCardType.Default,
             ),
         )
