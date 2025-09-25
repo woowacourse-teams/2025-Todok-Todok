9488d2933e9243fecdebcd32f389feeeca180471
Wooyoung <quantumjump10@gmail.com>
2025-09-25 11:11:10 +0900

    refactor: NotificationForbiddenException 전역 예외 처리 추가

--- backend/src/main/java/todoktodok/backend/notification/exception/NotificationForbiddenException.java
+++ backend/src/main/java/todoktodok/backend/notification/exception/NotificationForbiddenException.java
@@ -1,9 +1,5 @@
 package todoktodok.backend.notification.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
 public class NotificationForbiddenException extends RuntimeException {
     public NotificationForbiddenException(String message) {
         super(message);
