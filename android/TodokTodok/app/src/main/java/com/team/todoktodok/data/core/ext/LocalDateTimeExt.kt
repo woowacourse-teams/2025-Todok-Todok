e8f53bf0c202254d6b61574b4fde61e5ac6e7101
chanho0908 <uni070@naver.com>
2025-09-25 22:24:51 +0900

    refactor: #704 toLocalDate 확장 함수 제거

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/data/core/ext/LocalDateTimeExt.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/data/core/ext/LocalDateTimeExt.kt
@@ -1,12 +1,6 @@
 package com.team.todoktodok.data.core.ext
 
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 fun String.toLocalDateTime(): LocalDateTime = LocalDateTime.parse(this, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
-
-fun String.toLocalDate(): LocalDate =
-    LocalDateTime
-        .parse(this, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"))
-        .toLocalDate()
