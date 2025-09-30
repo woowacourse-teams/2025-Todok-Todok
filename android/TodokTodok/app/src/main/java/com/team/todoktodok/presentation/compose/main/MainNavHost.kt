5b7eab75083bbf4137b93e49559e3b810c979461
chanho0908 <uni070@naver.com>
2025-10-01 08:54:19 +0900

    feat: #732 ExceptionMessageConverter 의존성 추가

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/main/MainNavHost.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/main/MainNavHost.kt
@@ -2,27 +2,29 @@ package com.team.todoktodok.presentation.compose.main
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.team.todoktodok.presentation.compose.discussion.DiscussionsScreen
 import com.team.todoktodok.presentation.compose.my.MyScreen
+import com.team.todoktodok.presentation.core.ExceptionMessageConverter
 
 @Composable
 fun MainNavHost(
+    messageConverter: ExceptionMessageConverter,
     navController: NavHostController,
     startDestination: MainDestination,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     NavHost(
         navController = navController,
         startDestination = startDestination.route,
-        modifier = modifier
-    ){
+        modifier = modifier,
+    ) {
         MainDestination.entries.forEach { destination ->
-            composable(destination.route){
-                when(destination){
-                    MainDestination.Discussion -> DiscussionsScreen()
+            composable(destination.route) {
+                when (destination) {
+                    MainDestination.Discussion -> DiscussionsScreen(messageConverter)
                     MainDestination.My -> MyScreen()
                 }
             }
