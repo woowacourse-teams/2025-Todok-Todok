3d34c971bdda0804c3c4b36ac81dddcfa9714bf0
chanho0908 <uni070@naver.com>
2025-10-03 09:39:56 +0900

    refactor: #732 DiscussionTab 컴포저블을 DiscussionsScreen으로 통합

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/discussion/DiscussionsScreen.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/discussion/DiscussionsScreen.kt
@@ -1,58 +1,62 @@
 package com.team.todoktodok.presentation.compose.discussion
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.team.todoktodok.presentation.compose.LocalUiExceptionHandler
 import com.team.todoktodok.presentation.compose.UiExceptionHandler
 import com.team.todoktodok.presentation.compose.core.component.AlertSnackBar
-import com.team.todoktodok.presentation.compose.discussion.component.DiscussionTab
+import com.team.todoktodok.presentation.compose.core.component.DiscussionCardType
+import com.team.todoktodok.presentation.compose.discussion.all.AllDiscussionsScreen
+import com.team.todoktodok.presentation.compose.discussion.hot.HotDiscussionScreen
 import com.team.todoktodok.presentation.compose.discussion.model.AllDiscussionMode
+import com.team.todoktodok.presentation.compose.discussion.model.DiscussionTabStatus
+import com.team.todoktodok.presentation.compose.discussion.model.DiscussionTabStatus.Companion.DiscussionTabStatus
+import com.team.todoktodok.presentation.compose.discussion.search.SearchDiscussionsUiState
+import com.team.todoktodok.presentation.compose.theme.Green1A
+import com.team.todoktodok.presentation.compose.theme.Pretendard
+import com.team.todoktodok.presentation.compose.theme.White
 import com.team.todoktodok.presentation.core.ExceptionMessageConverter
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DiscussionsScreen(
     allDiscussionScreenMode: AllDiscussionMode,
+    searchDiscussionsUiState: SearchDiscussionsUiState,
     pagerState: PagerState,
     modifier: Modifier = Modifier,
 ) {
-    val coroutineScope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
-    var snackbarJob by remember { mutableStateOf<Job?>(null) }
+    val coroutineScope = rememberCoroutineScope()
 
-    val showMessage: (String, Long) -> Unit = { message, millis ->
-        snackbarJob?.cancel()
-        snackbarJob =
-            coroutineScope.launch {
-                val showJob =
-                    launch {
-                        snackbarHostState.showSnackbar(
-                            message = message,
-                            duration = SnackbarDuration.Indefinite,
-                        )
-                    }
-                delay(millis)
-                snackbarHostState.currentSnackbarData?.dismiss()
-                showJob.cancel()
-            }
-    }
     CompositionLocalProvider(
         LocalUiExceptionHandler provides
             UiExceptionHandler(
@@ -64,7 +68,74 @@ fun DiscussionsScreen(
             contentAlignment = Alignment.Center,
             modifier = modifier.fillMaxSize(),
         ) {
-            DiscussionTab(allDiscussionScreenMode = allDiscussionScreenMode, pagerState = pagerState)
+            Column(modifier = Modifier.fillMaxSize()) {
+                ScrollableTabRow(
+                    selectedTabIndex = pagerState.currentPage,
+                    indicator = { tabPositions ->
+                        Box(
+                            Modifier
+                                .tabIndicatorOffset(tabPositions[pagerState.currentPage])
+                                .height(4.dp)
+                                .padding(horizontal = 20.dp)
+                                .background(Green1A, RoundedCornerShape(50)),
+                        )
+                    },
+                    divider = {},
+                    containerColor = White,
+                    contentColor = Color.Black,
+                    edgePadding = 0.dp,
+                ) {
+                    DiscussionTabStatus.entries.forEachIndexed { index, tab ->
+                        Tab(
+                            text = {
+                                Text(
+                                    text = stringResource(tab.label),
+                                    fontFamily = Pretendard,
+                                    fontWeight = FontWeight.SemiBold,
+                                )
+                            },
+                            selected = pagerState.currentPage == index,
+                            modifier =
+                                Modifier
+                                    .width(100.dp)
+                                    .height(50.dp),
+                            onClick = {
+                                coroutineScope.launch {
+                                    pagerState.animateScrollToPage(index)
+                                }
+                            },
+                        )
+                    }
+                }
+
+                HorizontalDivider(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = Color.LightGray,
+                    thickness = 1.dp,
+                )
+
+                HorizontalPager(
+                    state = pagerState,
+                    modifier = Modifier.fillMaxSize(),
+                ) { page ->
+                    Box(
+                        modifier =
+                            Modifier
+                                .fillMaxSize()
+                                .background(White),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        when (DiscussionTabStatus(page)) {
+                            DiscussionTabStatus.HOT -> HotDiscussionScreen()
+                            DiscussionTabStatus.ALL ->
+                                AllDiscussionsScreen(
+                                    allDiscussionScreenMode = allDiscussionScreenMode,
+                                    searchDiscussion = searchDiscussionsUiState,
+                                )
+                        }
+                    }
+                }
+            }
 
             SnackbarHost(
                 hostState = snackbarHostState,
@@ -73,4 +144,20 @@ fun DiscussionsScreen(
             )
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+private fun DiscussionsScreenPreview() {
+    DiscussionsScreen(
+        allDiscussionScreenMode = AllDiscussionMode.LATEST,
+        searchDiscussionsUiState =
+            SearchDiscussionsUiState(
+                discussions = emptyList(),
+                type = DiscussionCardType.QueryHighlighting(""),
+                previousKeyword = "",
+            ),
+        pagerState = PagerState { 1 },
+    )
 }
