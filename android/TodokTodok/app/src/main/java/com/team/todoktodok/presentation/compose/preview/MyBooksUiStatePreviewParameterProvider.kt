82d83b1f62c9e0868cb858992fda45a80fa8e6e3
chanho0908 <uni070@naver.com>
2025-10-04 20:55:28 +0900

    feat: #747 MyBooksUiState PreviewParameterProvider 추가

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/preview/MyBooksUiStatePreviewParameterProvider.kt
@@ -0,0 +1,77 @@
+package com.team.todoktodok.presentation.compose.preview
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.team.domain.model.Book
+import com.team.todoktodok.presentation.compose.my.books.MyBooksUiState
+
+class MyBooksUiStatePreviewParameterProvider : PreviewParameterProvider<MyBooksUiState> {
+    override val values: Sequence<MyBooksUiState>
+        get() =
+            sequenceOf(
+                MyBooksUiState(
+                    books =
+                        listOf(
+                            Book(
+                                id = 1,
+                                title = "Effective Kotlin - Best Practices",
+                                author = "Marcin Moskala (JetBrains)",
+                                image = "",
+                            ),
+                            Book(
+                                id = 2,
+                                title = "Clean Code - A Handbook of Agile Software Craftsmanship",
+                                author = "Robert C. Martin (Uncle Bob)",
+                                image = "",
+                            ),
+                            Book(
+                                id = 3,
+                                title = "Android Programming - The Big Nerd Ranch Guide",
+                                author = "Bill Phillips (Big Nerd Ranch)",
+                                image = "",
+                            ),
+                            Book(
+                                id = 4,
+                                title = "Kotlin in Action",
+                                author = "Dmitry Jemerov (JetBrains)",
+                                image = "",
+                            ),
+                            Book(
+                                id = 5,
+                                title = "Refactoring - Improving the Design of Existing Code",
+                                author = "Martin Fowler (ThoughtWorks)",
+                                image = "",
+                            ),
+                            Book(
+                                id = 6,
+                                title = "Design Patterns - Elements of Reusable Object-Oriented Software",
+                                author = "Erich Gamma (GoF)",
+                                image = "",
+                            ),
+                            Book(
+                                id = 7,
+                                title = "You Don’t Know JS - ES6 & Beyond",
+                                author = "Kyle Simpson (O’Reilly)",
+                                image = "",
+                            ),
+                            Book(
+                                id = 8,
+                                title = "The Pragmatic Programmer - 20th Anniversary Edition",
+                                author = "Andy Hunt (PragProg)",
+                                image = "",
+                            ),
+                            Book(
+                                id = 9,
+                                title = "Head First Design Patterns",
+                                author = "Eric Freeman (O’Reilly)",
+                                image = "",
+                            ),
+                            Book(
+                                id = 10,
+                                title = "Test-Driven Development - By Example",
+                                author = "Kent Beck (XP)",
+                                image = "",
+                            ),
+                        ),
+                ),
+            )
+}
