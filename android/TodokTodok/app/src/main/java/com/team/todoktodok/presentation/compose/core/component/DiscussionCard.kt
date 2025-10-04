1b6ff998afc4a404c7821d68f9e24c47b7f81184
chanho0908 <uni070@naver.com>
2025-10-04 16:11:07 +0900

    fix: #747 토론 목록 카드에서 프로필 카드가 잘리는 문제 해결

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/core/component/DiscussionCard.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/core/component/DiscussionCard.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -226,6 +227,7 @@ private fun DiscussionBottom(
                     placeholder = painterResource(id = R.drawable.img_mascort),
                     error = painterResource(id = R.drawable.img_mascort),
                     fallback = painterResource(id = R.drawable.img_mascort),
+                    contentScale = ContentScale.Crop,
                     modifier =
                         Modifier
                             .clip(CircleShape)
