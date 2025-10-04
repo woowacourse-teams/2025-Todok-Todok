768d9cb85a93a37ff07e1e831cc3a3860f19d9b6
chanho0908 <uni070@naver.com>
2025-10-04 14:32:45 +0900

    feat: #747 프로필 화면 탭 컴포저블 추가

---
+++ android/TodokTodok/app/src/androidTest/java/com/team/todoktodok/compose/discussion/my/component/ProfileTabTest.kt
@@ -0,0 +1,25 @@
+package com.team.todoktodok.compose.discussion.my.component
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.team.todoktodok.presentation.compose.my.component.ProfileTab
+import org.junit.Rule
+import org.junit.Test
+
+class ProfileTabTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun `활동도서 좋아요 참여한 토론방 탭이 보인다`() {
+        // Given
+        composeTestRule.setContent {
+            ProfileTab()
+        }
+
+        composeTestRule.onNodeWithText("활동 도서").assertIsDisplayed()
+        composeTestRule.onNodeWithText("좋아요").assertIsDisplayed()
+        composeTestRule.onNodeWithText("참여한 토론방").assertIsDisplayed()
+    }
+}
