4f53aadf86b0f7bda74c7f69a5fb422c1e0a332f
chanho0908 <uni070@naver.com>
2025-10-05 09:17:56 +0900

    refactor: #747 MyBooksUiState를 MyBooksUiModel로 이름 변경

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/my/books/MyBooksUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.team.todoktodok.presentation.compose.my.books
-
-import com.team.domain.model.Book
-
-data class MyBooksUiState(
-    val books: List<Book> = emptyList(),
-) {
-    val notHasBooks = books.isEmpty()
-}
