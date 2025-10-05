d06dd7614425bf92a25c678809bfdf59334b5cb4
윤동현 <donghyunyoon81@gmail.com>
2025-10-05 22:46:51 +0900

    feat: #749 독서별 토론 화면 UI 구성

---
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/compose/bookdiscussions/components/BookDiscussionsSection.kt
@@ -0,0 +1,13 @@
+package com.team.todoktodok.presentation.compose.bookdiscussions.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.common.collect.ImmutableList
+import com.team.todoktodok.presentation.compose.bookdiscussions.model.DiscussionItem
+
+@Composable
+fun BookDiscussionsSection(
+    discussionItems: ImmutableList<DiscussionItem>,
+    modifier: Modifier = Modifier,
+) {
+}
