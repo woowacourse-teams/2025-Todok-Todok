4003156da55f27a9d7af61d971b1270d9fcc9190
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-10-06 20:59:12 +0900

    refactor: #766 사용하지 않는 에러 타입 삭제

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/discussion/create/ErrorCreateDiscussionType.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/discussion/create/ErrorCreateDiscussionType.kt
@@ -10,7 +10,6 @@ enum class ErrorCreateDiscussionType(
     CONTENT_NOT_FOUND(R.string.error_create_discussion_content_not_found),
     NOT_MY_DISCUSSION_ROOM(R.string.error_create_discussion_not_my_discussion_room),
     DISCUSSION_ROOM_INFO_NOT_FOUND(R.string.error_create_discussion_discussion_room_info_not_found),
-    PLEASE_TRY_AGAIN(R.string.error_create_discussion_please_try_again),
     TITLE_LENGTH_OVER(R.string.error_create_discussion_title_length_over),
     OPINION_LENGTH_OVER(R.string.error_create_discussion_opinion_length_over),
 
