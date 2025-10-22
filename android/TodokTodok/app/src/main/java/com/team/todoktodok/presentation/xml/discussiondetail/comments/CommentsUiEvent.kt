9178a7f272f1d987996580d45a9926b7d88507bb
윤동현 <donghyunyoon81@gmail.com>
2025-10-22 14:55:03 +0900

    refactor: 불필요한 import 제거

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/discussiondetail/comments/CommentsUiEvent.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/discussiondetail/comments/CommentsUiEvent.kt
@@ -1,7 +1,6 @@
 package com.team.todoktodok.presentation.xml.discussiondetail.comments
 
 import com.team.domain.model.exception.TodokTodokExceptions
-import com.team.todoktodok.presentation.xml.discussiondetail.DiscussionDetailUiEvent
 
 sealed interface CommentsUiEvent {
     data class ShowCommentCreate(
