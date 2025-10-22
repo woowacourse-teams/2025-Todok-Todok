12e9a49443c801762062d9f489c45f63286f00fa
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-10-22 17:13:08 +0900

    feat: #842 앱이 완전히 종료된 상태에서 알림을 클릭했을 때, 상세 화면으로 이동하도록 구현

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/adapter/FirebaseMessagingAdapter.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/adapter/FirebaseMessagingAdapter.kt
@@ -62,12 +62,12 @@ class FirebaseMessagingAdapter : FirebaseMessagingService() {
     ) {
         if (NotificationManagerCompat.from(this).areNotificationsEnabled() &&
             (
-                Build.VERSION.SDK_INT < 33 ||
-                    ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.POST_NOTIFICATIONS,
-                    ) == PackageManager.PERMISSION_GRANTED
-            )
+                    Build.VERSION.SDK_INT < 33 ||
+                            ActivityCompat.checkSelfPermission(
+                                this,
+                                Manifest.permission.POST_NOTIFICATIONS,
+                            ) == PackageManager.PERMISSION_GRANTED
+                    )
         ) {
             NotificationManagerCompat
                 .from(this)
