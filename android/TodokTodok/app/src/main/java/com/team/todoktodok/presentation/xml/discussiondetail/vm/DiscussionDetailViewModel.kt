181239652567568d9fb6a8e7bb4c4b10baf4a9de
윤동현 <donghyunyoon81@gmail.com>
2025-10-22 21:24:59 +0900

    refactor: 토론 상세 정보 조회 실패 시 UiState 변경 로직 수정

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/discussiondetail/vm/DiscussionDetailViewModel.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/discussiondetail/vm/DiscussionDetailViewModel.kt
@@ -218,7 +218,6 @@ class DiscussionDetailViewModel(
                         onUiEvent(
                             DiscussionDetailUiEvent.ShowErrorMessage(result.exception),
                         )
-                        _uiState.value = DiscussionDetailUiState.Failure(result.exception)
                     }
                 }
             }
