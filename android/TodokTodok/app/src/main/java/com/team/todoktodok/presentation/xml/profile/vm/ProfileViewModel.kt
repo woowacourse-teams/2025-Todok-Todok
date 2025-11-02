ba0f8984ebc5ecf0694e59c27db5427f409de421
chanho0908 <uni070@naver.com>
2025-11-02 21:50:09 +0900

    feat: #876 프로필 화면 화면 Hilt 적용 완료

--- android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/profile/vm/ProfileViewModel.kt
+++ android/TodokTodok/app/src/main/java/com/team/todoktodok/presentation/xml/profile/vm/ProfileViewModel.kt
@@ -20,150 +20,155 @@ import com.team.todoktodok.presentation.core.event.MutableSingleLiveData
 import com.team.todoktodok.presentation.core.event.SingleLiveData
 import com.team.todoktodok.presentation.xml.profile.ProfileUiEvent
 import com.team.todoktodok.presentation.xml.profile.ProfileUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ProfileViewModel(
-    private val memberRepository: MemberRepository,
-    private val tokenRepository: TokenRepository,
-) : ViewModel() {
-    private val _uiState = MutableLiveData(ProfileUiState())
-    val uiState: LiveData<ProfileUiState> get() = _uiState
+@HiltViewModel
+class ProfileViewModel
+    @Inject
+    constructor(
+        private val memberRepository: MemberRepository,
+        private val tokenRepository: TokenRepository,
+    ) : ViewModel() {
+        private val _uiState = MutableLiveData(ProfileUiState())
+        val uiState: LiveData<ProfileUiState> get() = _uiState
 
-    private val _uiEvent = MutableSingleLiveData<ProfileUiEvent>()
-    val uiEvent: SingleLiveData<ProfileUiEvent> get() = _uiEvent
+        private val _uiEvent = MutableSingleLiveData<ProfileUiEvent>()
+        val uiEvent: SingleLiveData<ProfileUiEvent> get() = _uiEvent
 
-    @Suppress("UNCHECKED_CAST")
-    fun loadProfile(id: Long) {
-        viewModelScope.launch {
-            val memberID = MemberId(id, tokenRepository.getMemberId())
-            _uiState.value = _uiState.value?.copy(memberId = memberID)
-            val memberId = _uiState.value?.memberId ?: return@launch
+        @Suppress("UNCHECKED_CAST")
+        fun loadProfile(id: Long) {
+            viewModelScope.launch {
+                val memberID = MemberId(id, tokenRepository.getMemberId())
+                _uiState.value = _uiState.value?.copy(memberId = memberID)
+                val memberId = _uiState.value?.memberId ?: return@launch
+                withLoading {
+                    val (profile, books, participatedDiscussions, createdDiscussions) =
+                        listOf(
+                            loadProfile(memberId),
+                            loadActivatedBooks(memberId),
+                            loadParticipatedDiscussions(memberId),
+                            loadCreatedDiscussions(memberId),
+                        ).awaitAll()
+
+                    _uiState.value =
+                        ProfileUiState.initial(
+                            memberId,
+                            profile as Profile,
+                            books as List<Book>,
+                            participatedDiscussions as List<Discussion>,
+                            createdDiscussions as List<Discussion>,
+                        )
+                }
+            }
+        }
+
+        private fun loadProfile(id: MemberId): Deferred<Profile> =
+            viewModelScope.async {
+                when (val result = memberRepository.getProfile(id)) {
+                    is NetworkResult.Success -> result.data
+                    is NetworkResult.Failure -> {
+                        onUiEvent(ProfileUiEvent.ShowErrorMessage(result.exception))
+                        Profile.EMPTY
+                    }
+                }
+            }
+
+        private fun loadActivatedBooks(id: MemberId): Deferred<List<Book>> =
+            viewModelScope.async {
+                when (val result = memberRepository.getMemberBooks(id)) {
+                    is NetworkResult.Success -> result.data
+                    is NetworkResult.Failure -> {
+                        onUiEvent(ProfileUiEvent.ShowErrorMessage(result.exception))
+                        emptyList()
+                    }
+                }
+            }
+
+        private fun loadParticipatedDiscussions(id: MemberId): Deferred<List<Discussion>> =
+            viewModelScope.async {
+                when (
+                    val result =
+                        memberRepository.getMemberDiscussionRooms(id, MemberDiscussionType.PARTICIPATED)
+                ) {
+                    is NetworkResult.Success -> result.data
+                    is NetworkResult.Failure -> {
+                        onUiEvent(ProfileUiEvent.ShowErrorMessage(result.exception))
+                        emptyList()
+                    }
+                }
+            }
+
+        private fun loadCreatedDiscussions(id: MemberId): Deferred<List<Discussion>> =
+            viewModelScope.async {
+                when (
+                    val result =
+                        memberRepository.getMemberDiscussionRooms(id, MemberDiscussionType.CREATED)
+                ) {
+                    is NetworkResult.Success -> result.data
+                    is NetworkResult.Failure -> {
+                        onUiEvent(ProfileUiEvent.ShowErrorMessage(result.exception))
+                        emptyList()
+                    }
+                }
+            }
+
+        fun supportMember(
+            type: Support,
+            reason: String,
+        ) {
             withLoading {
-                val (profile, books, participatedDiscussions, createdDiscussions) =
+                val memberId = _uiState.value?.memberId
+                if (memberId is MemberId.OtherUser) {
+                    memberRepository
+                        .supportMember(memberId, type, reason)
+                        .onSuccess { onUiEvent(ProfileUiEvent.OnCompleteSupport(type)) }
+                        .onFailure { onUiEvent(ProfileUiEvent.ShowErrorMessage(it)) }
+                }
+            }
+        }
+
+        fun refreshProfile() {
+            val memberId = _uiState.value?.memberId ?: return
+            withLoading {
+                val profile = loadProfile(memberId)
+                _uiState.value = _uiState.value?.modifyProfile(profile.await())
+            }
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        fun refreshUserActivities() {
+            val memberId = _uiState.value?.memberId ?: return
+            withLoading {
+                val (books, participatedDiscussions, createdDiscussions) =
                     listOf(
-                        loadProfile(memberId),
                         loadActivatedBooks(memberId),
                         loadParticipatedDiscussions(memberId),
                         loadCreatedDiscussions(memberId),
                     ).awaitAll()
 
                 _uiState.value =
-                    ProfileUiState.initial(
-                        memberId,
-                        profile as Profile,
+                    _uiState.value?.modifyActivities(
                         books as List<Book>,
                         participatedDiscussions as List<Discussion>,
                         createdDiscussions as List<Discussion>,
                     )
             }
         }
-    }
 
-    private fun loadProfile(id: MemberId): Deferred<Profile> =
-        viewModelScope.async {
-            when (val result = memberRepository.getProfile(id)) {
-                is NetworkResult.Success -> result.data
-                is NetworkResult.Failure -> {
-                    onUiEvent(ProfileUiEvent.ShowErrorMessage(result.exception))
-                    Profile.EMPTY
-                }
+        private fun withLoading(action: suspend () -> Unit) {
+            viewModelScope.launch {
+                _uiState.value = _uiState.value?.toggleLoading(true)
+                action()
+                _uiState.value = _uiState.value?.toggleLoading(false)
             }
         }
 
-    private fun loadActivatedBooks(id: MemberId): Deferred<List<Book>> =
-        viewModelScope.async {
-            when (val result = memberRepository.getMemberBooks(id)) {
-                is NetworkResult.Success -> result.data
-                is NetworkResult.Failure -> {
-                    onUiEvent(ProfileUiEvent.ShowErrorMessage(result.exception))
-                    emptyList()
-                }
-            }
-        }
-
-    private fun loadParticipatedDiscussions(id: MemberId): Deferred<List<Discussion>> =
-        viewModelScope.async {
-            when (
-                val result =
-                    memberRepository.getMemberDiscussionRooms(id, MemberDiscussionType.PARTICIPATED)
-            ) {
-                is NetworkResult.Success -> result.data
-                is NetworkResult.Failure -> {
-                    onUiEvent(ProfileUiEvent.ShowErrorMessage(result.exception))
-                    emptyList()
-                }
-            }
-        }
-
-    private fun loadCreatedDiscussions(id: MemberId): Deferred<List<Discussion>> =
-        viewModelScope.async {
-            when (
-                val result =
-                    memberRepository.getMemberDiscussionRooms(id, MemberDiscussionType.CREATED)
-            ) {
-                is NetworkResult.Success -> result.data
-                is NetworkResult.Failure -> {
-                    onUiEvent(ProfileUiEvent.ShowErrorMessage(result.exception))
-                    emptyList()
-                }
-            }
-        }
-
-    fun supportMember(
-        type: Support,
-        reason: String,
-    ) {
-        withLoading {
-            val memberId = _uiState.value?.memberId
-            if (memberId is MemberId.OtherUser) {
-                memberRepository
-                    .supportMember(memberId, type, reason)
-                    .onSuccess { onUiEvent(ProfileUiEvent.OnCompleteSupport(type)) }
-                    .onFailure { onUiEvent(ProfileUiEvent.ShowErrorMessage(it)) }
-            }
+        private fun onUiEvent(event: ProfileUiEvent) {
+            _uiEvent.setValue(event)
         }
     }
-
-    fun refreshProfile() {
-        val memberId = _uiState.value?.memberId ?: return
-        withLoading {
-            val profile = loadProfile(memberId)
-            _uiState.value = _uiState.value?.modifyProfile(profile.await())
-        }
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    fun refreshUserActivities() {
-        val memberId = _uiState.value?.memberId ?: return
-        withLoading {
-            val (books, participatedDiscussions, createdDiscussions) =
-                listOf(
-                    loadActivatedBooks(memberId),
-                    loadParticipatedDiscussions(memberId),
-                    loadCreatedDiscussions(memberId),
-                ).awaitAll()
-
-            _uiState.value =
-                _uiState.value?.modifyActivities(
-                    books as List<Book>,
-                    participatedDiscussions as List<Discussion>,
-                    createdDiscussions as List<Discussion>,
-                )
-        }
-    }
-
-    private fun withLoading(action: suspend () -> Unit) {
-        viewModelScope.launch {
-            _uiState.value = _uiState.value?.toggleLoading(true)
-            action()
-            _uiState.value = _uiState.value?.toggleLoading(false)
-        }
-    }
-
-    private fun onUiEvent(event: ProfileUiEvent) {
-        _uiEvent.setValue(event)
-    }
-}
