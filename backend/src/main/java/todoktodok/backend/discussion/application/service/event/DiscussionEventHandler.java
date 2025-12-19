70771b4faa248ce98c8308c98a73cfb2f1b15465
ljhee92 <weilainib7@gmail.com>
2025-12-19 23:29:04 +0900

    fix: 비동기 방식 원복

--- backend/src/main/java/todoktodok/backend/discussion/application/service/event/DiscussionEventHandler.java
+++ backend/src/main/java/todoktodok/backend/discussion/application/service/event/DiscussionEventHandler.java
@@ -3,6 +3,7 @@ package todoktodok.backend.discussion.application.service.event;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -80,9 +81,9 @@ public class DiscussionEventHandler {
         fcmPushNotifier.sendPush(recipientId, fcmMessagePayload);
     }
 
-//    @Async
-//    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    @EventListener
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+//    @EventListener
     public void handleDiscussionView(final DiscussionViewEvent discussionViewEvent) {
         try {
             log.info("DiscussionViewEvent 실행");
